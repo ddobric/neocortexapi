52a9d87b89fa62e5df7a8d632f7d5af08b82b0ff
Ataulhaye Ahmad <aahmad@daenet.com>
2018-06-07 17:41:26 +0200

    added picture and some changes

--- NeuroVizualizer/test-highchart/src/app/scattered3-dchart/scattered3-dchart.component.ts
+++ NeuroVizualizer/test-highchart/src/app/scattered3-dchart/scattered3-dchart.component.ts
@@ -8,6 +8,7 @@ const Highcharts = require('highcharts');
 Highcharts.setOptions({
   //colors: ['#50B432'],
   redraw: false,
+
   
 });
 
@@ -24,6 +25,7 @@ export class Scattered3DchartComponent implements OnInit {
   z: any=252;
   n:any = 10000;
   i: any;
+  //redraw: boolean;
   
     me: any;
 
@@ -33,11 +35,12 @@ export class Scattered3DchartComponent implements OnInit {
   constructor() { 
      
     this.me = this.chartOpts;
+     // this.chartOpts.chart.redraw = false;
   }
 
   ngOnInit() {
 
-    this.initData(5,5,5);
+    this.initData(10,10,10);
     this.setOptions(null);   
   
     }
@@ -51,7 +54,6 @@ export class Scattered3DchartComponent implements OnInit {
     exporting: { enabled: false },
     credits: { enabled: false },
       chart: {
-          
         height: 600,
         zoomType: 'xy',
         margin: 100,
@@ -82,7 +84,7 @@ export class Scattered3DchartComponent implements OnInit {
             width: 10,
             height: 10,
             depth: 10,
-            //groupPadding: 40
+            //groupPadding: 0
         }
     },
     yAxis: {
@@ -106,6 +108,7 @@ export class Scattered3DchartComponent implements OnInit {
     series: [{
         name: 'Reading',
         colorByPoint: true,
+        redraw: false,
         marker:{
             symbol: 'url(../../../assets/images/cylinder.png)',
             width: 30,
@@ -113,58 +116,14 @@ export class Scattered3DchartComponent implements OnInit {
         }
        , 
        data:this.data,
-        //data: [
-            /*
-                [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5], [0, 0, 6], [0, 0, 7], [0, 0, 8],
-                [1, 1, 1], [1, 1, 2], [1, 1, 3], [1, 1, 4], [1, 1, 5], [1, 1, 6], [1, 1, 7], [1, 1, 8],
-                [2, 2, 1], [2, 2, 2], [2, 2, 3], [2, 2, 4], [2, 2, 5], [2, 2, 6], [2, 2, 7], [2, 2, 8],
-                [3, 3, 1], [3, 3, 2], [3, 3, 3], [3, 3, 4], [3, 3, 5], [3, 3, 6], [3, 3, 7], [3, 3, 8],
-                [4, 4, 1], [4, 4, 2], [4, 4, 3], [4, 4, 4], [4, 4, 5], [4, 4, 6], [4, 4, 7], [4, 4, 8],
-                [5, 5, 1], [5, 5, 2], [5, 5, 3], [5, 5, 4], [5, 5, 5], [5, 5, 6], [5, 5, 7], [5, 5, 8],
-                [6, 6, 1], [6, 6, 2], [6, 6, 3], [6, 6, 4], [6, 6, 5], [6, 6, 6], [6, 6, 7], [6, 6, 8],
-                [7, 7, 1], [7, 7, 2], [7, 7, 3], [7, 7, 4], [7, 7, 5], [7, 7, 6], [7, 7, 7], [7, 7, 8],
-
-                [1, 0, 0], [2, 0, 0], [3, 0, 0], [4, 0, 0], [5, 0, 0], [6, 0, 0], [7, 0, 0], [8, 0, 0],
-                [1, 1, 0], [2, 2, 0], [3, 3, 0], [4, 4, 0], [5, 5, 0], [6, 6, 0], [7, 7, 0], [8, 8, 0],
-                */
-                
-
-
-           /* [7, 0, 1], [2, 1, 1], [3, 2, 1], [4, 3, 1], [5, 4, 6], [6, 5, 1],
-            [1, 0, 2], [2, 1, 2], [3, 2, 2], [4, 3, 2], [5, 4, 2], [6, 5, 2],
-            [7, 0, 3], [2, 1, 3], [3, 2, 3], [4, 3, 3], [5, 4, 3], [6, 5, 3],
-            [2, 6, 1], [8, 9, 2], [7, 6, 5], [6, 3, 1], [9, 3, 1], [8, 9, 3],
-            [9, 1, 0], [3, 8, 7], [8, 0, 0], [4, 9, 7], [8, 6, 2], [4, 3, 0],
-            [2, 3, 5], [9, 1, 4], [1, 1, 4], [6, 0, 2], [6, 1, 6], [3, 8, 8],
-            [8, 8, 7], [5, 5, 0], [3, 9, 6], [5, 4, 3], [6, 8, 3], [0, 1, 5],
-            [6, 7, 3], [8, 3, 2], [3, 8, 3], [2, 1, 6], [4, 6, 7], [8, 9, 9],
-            [5, 4, 2], [6, 1, 3], [6, 9, 5], [4, 8, 2], [9, 7, 4], [5, 4, 2],
-            [9, 6, 1], [2, 7, 3], [4, 5, 4], [6, 8, 1], [3, 4, 0], [2, 2, 6],
-            [5, 1, 2], [9, 9, 7], [6, 9, 9], [8, 4, 3], [4, 1, 7], [6, 2, 5],
-            [0, 4, 9], [3, 5, 9], [6, 9, 1], [3, 9, 2]
-            */
-          // ]
     }]
       
   };
   }
-/*
-  insertData(){
-for (this.i=0; this.i < this.n; this.i += 1) {
-    this.sampledata.push([
-        Math.pow(Math.random(), 1) * 100,
-        Math.pow(Math.random(), 2) * 100,
-        Math.pow(Math.random(), 3) * 100
-    ]);
-this.onInputChange(null);   
-  }
-}
+ 
 
-*/
 initData(xDim,zDim,yDim){
-
     //this.chartOpts.series[0].data = [];
-
     var x;
     var y;
     var z;
@@ -175,18 +134,14 @@ initData(xDim,zDim,yDim){
                  x,
                  y,
                  z
-             ]);
-             
-             //this.onInputChange(null);      
+             ]);     
          }
          
      }
      
    }
-   //console.log('[\n    [' + this.sampledata.join('],\n    [') + ']\n]');
   
    }
-
-
-
 }
+
+
