7ea237eac51a38bdf6f85e4161faad5f2cedf215
Damir Dobric <ddobric@daenet.com>
2018-07-31 14:20:05 +0200

    progress

--- NeuroVizualizer/test-highchart/src/app/neocortexmodel.ts
+++ NeuroVizualizer/test-highchart/src/app/neocortexmodel.ts
@@ -7,12 +7,19 @@ export class neocortexSettings{
   public numAreas: number;
   public minicolumnDims: number[];
   public numCellsInMinicolumn: number;
+  public inputModel : InputModel;
 }
 
 export class NeoCortexModel {
 
   public areas: Array<Area>;
   
+  public synapses: Array<Synapse>;
+  
+  public cells: Array<Cell>;
+  
+  public input: InputModel;
+
   constructor(settings:neocortexSettings ) {
      this.areas = new Array(settings.numAreas);
      for(var i=0;i<settings.numAreas;i++)
@@ -27,6 +34,8 @@ export class Area {
 
   public minicolumns: Minicolumn[][] = new Array();
   
+  public id:number;
+
   constructor(minicolumns: number[]= [3,2048], cellsInMinicolumn:number) {
 
     //this.minicolumns = new Minicolumn[minicolumns[0]][minicolumns[1]];
@@ -49,10 +58,13 @@ export class Area {
 
 export class Minicolumn {
 
-  public Cells: Cell[];
+  public cells: Array<Cell> = new Array();
   
   constructor(numCells: number) {
-     
+
+    //todo. create cells and push them to the list.
+    let cell : Cell = new Cell(1);
+    this.cells.push(cell);
   }
   
 }
@@ -70,31 +82,71 @@ export class location{
   
 }
 
+
+/**
+ * 
+ */
 export class Cell extends location {
 
   public Synapses: Synapse[];
   
   public Layer: number;
 
-  constructor(posX:number = 0, posY:number=0, posZ:number=0) {
-   super(posX, posY, posY);
+/**
+ * 
+ * @param layer Optional for input model.
+ * @param posX 
+ * @param posY 
+ * @param posZ 
+ */
+  constructor(layer:number=0,posX:number = 0, posY:number=0, posZ:number=0) {
+       super(posX, posY, posY);
+       this.Layer=layer;
   }    
 }
 
 
 export class Synapse {
 
+  public id:number;
+
   public preSynaptic: Cell;
 
   public postSynaptic: Cell;
   
-  constructor(preSynaptic: Cell, postSynaptic: Cell) {
+  public permanence: number;
+
+  constructor(id:number, permanence:number = 0, preSynaptic: Cell, postSynaptic: Cell) {
       this.preSynaptic = preSynaptic;
       this.postSynaptic = postSynaptic;
   }
   
 }
 
+
+export class InputModel {
+
+  public cells: Cell[][] = new Array();
+  
+  public id:number;
+
+  constructor(cellDims: number[]= [1,2048]) {
+
+    //this.minicolumns = new Minicolumn[minicolumns[0]][minicolumns[1]];
+    this.cells = new Array();
+
+    for (var i = 0; i < cellDims[0]; i++) {
+
+      let row : Array<Cell> = new Array();
+      
+      for (var j = 0; j < cellDims[1]; j++) {
+        row.push(new Cell());       
+      }
+
+      this.cells.push(row);
+    }
+  }  
+}
 
 
 
--- NeuroVizualizer/test-highchart/src/app/neocortexutils.ts
+++ NeuroVizualizer/test-highchart/src/app/neocortexutils.ts
@@ -1,30 +1,99 @@
-import { NeoCortexModel, Area, Synapse, Minicolumn, Cell, neocortexSettings } from './neocortexmodel';
+import { NeoCortexModel, Area, Synapse, Minicolumn, Cell, neocortexSettings, InputModel } from './neocortexmodel';
 
 
 
 
 export class neoCortexUtils {
 
- 
+
   /**
    * 
    * @param areas 
    * @param minicolumns 
    * @param cellsInMinicolumn 
    */
-  public static createModel(areas: number=1, minicolumns: number[]= [1000,3], cellsInMinicolumn: number=6) : NeoCortexModel { 
+  public static createModel(areas: number = 1, minicolumns: number[] = [1000, 3], cellsInMinicolumn: number = 6): NeoCortexModel {
 
-    var  sett :  neocortexSettings = { numAreas: areas, minicolumnDims: minicolumns, numCellsInMinicolumn: cellsInMinicolumn  };
+    let inpModel : InputModel = new InputModel([3,3]);
 
-    var  model:NeoCortexModel = new NeoCortexModel(sett);
+    let sett: neocortexSettings = {numAreas: areas, minicolumnDims: minicolumns, numCellsInMinicolumn: cellsInMinicolumn, inputModel: inpModel };
 
-    return model; 
+    var model: NeoCortexModel = new NeoCortexModel(sett);
+
+   // this.addSynapse
+    return model;
   }
 
 
-  public static updateSynapse(posX : number, posY: number, permanence : number)  { 
+  public static addSynapse(model: NeoCortexModel, synapseId: number, areaId: number = -1, preCellId:number, postCellId:number, weight: number) {
 
-    
+    let synapse = this.lookupSynapse(model, synapseId, areaId);
+    if (synapse != null) {
+      synapse.permanence = weight;
+    }
+    else
+      throw "Synapse cannot be found!";
+
+  }
+
+  public static updateSynapse(model: NeoCortexModel, synapseId: number, areaId: number = -1, weight: number) {
+
+    let synapse = this.lookupSynapse(model, synapseId, areaId);
+    if (synapse != null) {
+      synapse.permanence = weight;
+    }
+    else
+      throw "Synapse cannot be found!";
+
+  }
+
+  public static updateNeuron(model: NeoCortexModel, id: number, weight: number) {
+
+
+  }
+
+
+  /**
+   * 
+   * @param model 
+   * @param synapseId 
+   * @param [optional] areaId.
+   */
+  public static lookupSynapse(model: NeoCortexModel, synapseId: number, areaId: number = -1): Synapse {
+
+    if (areaId >= 0 && model.areas.length > areaId)
+      return this.lookupSynapseInArea(model, synapseId, areaId);
+
+    model.areas.forEach(area => {
+      let synapse = this.lookupSynapseInArea(model, synapseId, area.id);
+      if (synapse != null)
+        return synapse;
+    });
+
+    return null;
+  }
+
+
+  /**
+   * 
+   * @param model 
+   * @param synapseId 
+   * @param areaId 
+   */
+  private static lookupSynapseInArea(model: NeoCortexModel, synapseId: number, areaId: number): Synapse {
+
+    model.areas[areaId].minicolumns.forEach(minColRow => {
+      minColRow.forEach(miniColumn => {
+        miniColumn.cells.forEach(cell => {
+          cell.Synapses.forEach(synapse => {
+            if (synapse.id == synapseId)
+              return synapse;
+          });
+        });
+      });
+    });
+
+    return null;
   }
 }
 
