e0392f4d431e28c9901cf167e3d1322bece14103
Damir Dobric <ddobric@daenet.com>
2019-01-07 20:49:44 +0100

    Started with network. Classification algorithm added.

---
+++ NeoCortexApi/NeoCortexApi/Network/IInference.cs
@@ -0,0 +1,105 @@
+﻿using NeoCortexApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeoCortexApi.Network
+{
+    public interface IInference
+    {
+        /**
+     * Returns the input record sequence number associated with 
+     * the state of a {@link Layer} which this {@code Inference}
+     * represents.
+     * 
+     * @return
+     */
+         int getRecordNum();
+        /**
+         * Returns the {@link ComputeCycle}
+         * @return
+         */
+         ComputeCycle getComputeCycle();
+        /**
+         * Returns a custom Object during sequence processing where one or more 
+         * {@link Func1}(s) were added to a {@link Layer} in between algorithmic
+         * components.
+         *  
+         * @return  the custom object set during processing
+         */
+         Object getCustomObject();
+        /**
+         * Returns the {@link Map} used as input into a given {@link CLAClassifier}
+         * if it exists.
+         * 
+         * @return
+         */
+         Dictionary<String, object /*NamedTuple*/> getClassifierInput();
+        /**
+         * Returns a tuple containing key/value pairings of input field
+         * names to the {@link CLAClassifier} used in conjunction with it.
+         * 
+         * @return
+         */
+         object /*NamedTuple*/ getClassifiers();
+        /**
+         * Returns the object used as input into a given Layer
+         * which is associated with this computation result.
+         * @return
+         */
+         Object getLayerInput();
+
+        /**
+         * Returns the <em>Sparse Distributed Representation</em> vector
+         * which is the result of all algorithms in a series of algorithms
+         * passed up the hierarchy.
+         * 
+         * @return
+         */
+         int[] getSDR();
+        /**
+         * Returns the initial encoding produced by an {@link Encoder} or one
+         * of its subtypes.
+         * 
+         * @return
+         */
+         int[] getEncoding();
+        /**
+         * Returns the most recent {@link Classification}
+         * 
+         * @param fieldName
+         * @return
+         */
+         Classification<Object> getClassification(String fieldName);
+        /**
+         * Returns the most recent anomaly calculation.
+         * @return
+         */
+         double getAnomalyScore();
+        /**
+         * Returns the column activation from a {@link SpatialPooler}
+         * @return
+         */
+         int[] getFeedForwardActiveColumns();
+        /**
+         * Returns the column activations in sparse form
+         * @return
+         */
+         int[] getFeedForwardSparseActives();
+        /**
+         * Returns the column activation from a {@link TemporalMemory}
+         * @return
+         */
+         Set<Cell> getActiveCells();
+        /**
+         * Returns the predicted output from the last inference cycle.
+         * @return
+         */
+         Set<Cell> getPreviousPredictiveCells();
+        /**
+         * Returns the currently predicted columns.
+         * @return
+         */
+         Set<Cell> getPredictiveCells();
+    }
+}
---
+++ NeoCortexApi/NeoCortexApi/Network/Layer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeoCortexApi
+{
+    /**
+ * <p>
+ * Implementation of the biological layer of a region in the neocortex. Here, a
+ * {@code Layer} contains the physical structure (columns, cells, dendrites etc)
+ * shared by a sequence of algorithms which serve to implement the predictive
+ * inferencing present in this, the allegory to its biological equivalent.
+ * </p>
+ * <p>
+ * <b>COMPOSITION:</b> A Layer is constructed with {@link Parameters} which
+ * configure the behavior of most of the key algorithms a Layer may contain. It
+ * is also <em>optionally</em> constructed with each of the algorithms in turn.
+ * A Layer that includes an {@link Encoder} is always initially configured with
+ * a {@link MultiEncoder}. The child encoders contained within the MultiEncoder
+ * are configured from the Map included with the specified Parameters, keyed by
+ * {@link Parameters.KEY#FIELD_ENCODING_MAP}.
+ * </p>
+ * <p>
+ * A field encoding map consists of one map for each of the fields to be
+ * encoded. Each individual map in the field encoding map contains the typical
+ * {@link Encoder} parameters, plus a few "meta" parameters needed to describe
+ * the field and its data type as follows:
+ * </p>
+ * 
+ * <pre>
+ *      Map&lt;String, Map&lt;String, Object&gt;&gt; fieldEncodings = new HashMap&lt;&gt;();
+ *      
+ *      Map&lt;String, Object&gt; inner = new HashMap&lt;&gt;();
+ *      inner.put("n", n);
+ *      inner.put("w", w);
+ *      inner.put("minVal", min);
+ *      inner.put("maxVal", max);
+ *      inner.put("radius", radius);
+ *      inner.put("resolution", resolution);
+ *      inner.put("periodic", periodic);
+ *      inner.put("clip", clip);
+ *      inner.put("forced", forced);
+ *      // These are meta info to aid in Encoder construction
+ *      inner.put("fieldName", fieldName);
+ *      inner.put("fieldType", fieldType); (see {@link FieldMetaType} for type examples)
+ *      inner.put("encoderType", encoderType); (i.e. ScalarEncoder, SDRCategoryEncoder, DateEncoder...etc.)
+ *      
+ *      Map&lt;String, Object&gt; inner2 = new HashMap&lt;&gt;();
+ *      inner.put("n", n);
+ *      inner.put("w", w);
+ *      inner.put("minVal", min);
+ *      inner.put("maxVal", max);
+ *      inner.put("radius", radius);
+ *      inner.put("resolution", resolution);
+ *      inner.put("periodic", periodic);
+ *      inner.put("clip", clip);
+ *      inner.put("forced", forced);
+ *      // These are meta info to aid in Encoder construction
+ *      inner.put("fieldName", fieldName);
+ *      inner.put("fieldType", fieldType); (see {@link FieldMetaType} for type examples)
+ *      inner.put("encoderType", encoderType); (i.e. ScalarEncoder, SDRCategoryEncoder, DateEncoder...etc.)
+ *      
+ *      fieldEncodings.put("consumption", inner);  // Where "consumption" is an example field name (field name is "generic" in above code)
+ *      fieldEncodings.put("temperature", inner2);
+ *      
+ *      Parameters p = Parameters.getDefaultParameters();
+ *      p.setParameterByKey(KEY.FIELD_ENCODING_MAP, fieldEncodings);
+ * </pre>
+ * 
+ * For an example of how to create the field encodings map in a reusable way,
+ * see NetworkTestHarness and its usage within the LayerTest class.
+ * 
+ * <p>
+ * The following is an example of Layer construction with everything included
+ * (i.e. Sensor, SpatialPooler, TemporalMemory, CLAClassifier, Anomaly
+ * (computer))
+ * 
+ * <pre>
+ * // See the test harness for more information
+ * Parameters p = NetworkTestHarness.getParameters();
+ * 
+ * // How to merge (union) two {@link Parameters} objects. This one merges
+ * // the Encoder parameters into default parameters.
+ * p = p.union(NetworkTestHarness.getHotGymTestEncoderParams());
+ * 
+ * // You can overwrite parameters as needed like this
+ * p.setParameterByKey(KEY.RANDOM, new MersenneTwister(42));
+ * p.setParameterByKey(KEY.COLUMN_DIMENSIONS, new int[] { 2048 });
+ * p.setParameterByKey(KEY.POTENTIAL_RADIUS, 200);
+ * p.setParameterByKey(KEY.INHIBITION_RADIUS, 50);
+ * p.setParameterByKey(KEY.GLOBAL_INHIBITIONS, true);
+ * 
+ * Map&lt;String, Object&gt; params = new HashMap&lt;&gt;();
+ * params.put(KEY_MODE, Mode.PURE);
+ * params.put(KEY_WINDOW_SIZE, 3);
+ * params.put(KEY_USE_MOVING_AVG, true);
+ * Anomaly anomalyComputer = Anomaly.create(params);
+ * 
+ * Layer&lt;?&gt; l = Network.createLayer(&quot;TestLayer&quot;, p).alterParameter(KEY.AUTO_CLASSIFY, true).add(anomalyComputer).add(new TemporalMemory()).add(new SpatialPooler())
+ *                 .add(Sensor.create(FileSensor::create, SensorParams.create(Keys::path, &quot;&quot;, ResourceLocator.path(&quot;rec-center-hourly-small.csv&quot;))));
+ * </pre>
+ * 
+ * 
+ * 
+ * 
+ * @author David Ray
+ */
+    public class Layer
+    {
+    }
+}
