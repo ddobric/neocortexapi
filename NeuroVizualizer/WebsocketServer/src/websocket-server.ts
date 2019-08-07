271b4d72559afa2813c810891f1dccc0a2816006
Ataulhaye Ahmad <aahmad@daenet.com>
2019-08-07 15:26:41 +0200

    implemented WebSocket Server

---
+++ NeuroVizualizer/WebsocketServer/src/websocket-server.ts
@@ -0,0 +1,54 @@
+import { createServer, Server } from 'http';
+import * as express from 'express';
+import * as socketIo from 'socket.io';
+
+
+export class WebsocketServer {
+    public static readonly PORT:number = 8080;
+    private app: express.Application;
+    private server: Server;
+    private io: SocketIO.Server;
+    private port: string | number;
+
+    constructor() {
+        this.createApp();
+        this.config();
+        this.createServer();
+        this.sockets();
+        this.listen();
+    }
+
+    private createApp(): void {
+        this.app = express();
+    }
+
+    private createServer(): void {
+        this.server = createServer(this.app);
+    }
+
+    private config(): void {
+        this.port = process.env.PORT || WebsocketServer.PORT;
+    }
+
+    private sockets(): void {
+        this.io = socketIo(this.server);
+    }
+
+    private listen(): void {
+        this.server.listen(this.port, () => {
+            console.log('Running server on port %s', this.port);
+        });
+
+        this.io.on('connect', (socket: any) => {
+            console.log('Connected client on port %s.', this.port);
+
+            socket.on('disconnect', () => {
+                console.log('Client disconnected');
+            });
+        });
+    }
+
+    public getApp(): express.Application {
+        return this.app;
+    }
+}
