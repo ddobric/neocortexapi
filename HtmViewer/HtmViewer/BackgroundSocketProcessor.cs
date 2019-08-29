5d0b29fa17cb68d5cfade7fb423a62b1f8cd8fb1
Ataulhaye Ahmad <aahmad@daenet.com>
2019-08-29 17:47:14 +0200

    added new Project websocket server

---
+++ HtmViewer/HtmViewer/BackgroundSocketProcessor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HtmViewer
+{
+    public class BackgroundSocketProcessor
+    {
+        public static async Task SendMsgToWebApp(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+
+        internal static void AddSocket(WebSocket socket, TaskCompletionSource<object> socketFinishedTcs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HtmViewer/HtmViewer/Controllers/WebSocketController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.Web.CodeGeneration;
+
+namespace HtmViewer.Controllers
+{
+    [Route("/ws")]
+    public class WebSocketController : Controller
+    {
+        ConcurrentDictionary<string, string> sessions = new ConcurrentDictionary<string, string>();
+
+        private ILogger<WebSocketController> m_Logger;
+
+        public WebSocketController(ILogger<WebSocketController> logger)
+        {
+            m_Logger = logger;
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            var context = ControllerContext.HttpContext;
+
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                m_Logger.LogInformation("Websocket conntect from {ip}", context.Connection.RemoteIpAddress);
+                m_Logger.LogInformation("Websocket conntect with following ID {}", context.Connection.Id);
+                m_Logger.LogInformation("Client Message");
+                await SendMsgToWebApp(context, webSocket);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+            }
+        }
+
+        public static async Task SendMsgToWebApp(HttpContext context, WebSocket webSocket)
+        {
+            //{ "msgType": "init", "data": { "clientType": "NeuroVisualizer"} ""}
+           // ws://localhost:5000/ws
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+
+        }
+
+    }
+}
