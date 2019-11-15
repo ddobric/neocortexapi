bb557f8078da54f76b97082577ff0b446279a854
Ataulhaye Ahmad <aahmad@daenet.com>
2019-11-15 17:26:23 +0100

    implemented websocket client methods

--- NeoCortexApi/WebSocketNeuroVisualizer/WsVisualizer.cs
+++ NeoCortexApi/WebSocketNeuroVisualizer/WsVisualizer.cs
@@ -1,13 +1,20 @@
 ﻿using NeoCortexApi.Entities;
 using NeoCortexEntities.NeuroVisualizer;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
 namespace WebSocketNeuroVisualizer
 {
+ 
     public class WSNeuroVisualizer : INeuroVisualizer
     {
+        string url = "ws://localhost:5011/ws/client1";
+
         public Task InitModel(NeuroModel model)
         {
             throw new NotImplementedException();
@@ -38,5 +45,32 @@ namespace WebSocketNeuroVisualizer
             }
             
         }
+        public async Task Connect(string url, CancellationToken cancellationToken)
+        {
+            try
+            {
+                ClientWebSocket websocket = new ClientWebSocket();
+                await websocket.ConnectAsync((new Uri(url)), CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex);
+            }
+
+
+        }
+
+        public async Task SendData(ClientWebSocket websocket, string message, bool endOfMessage)
+        {
+            if (websocket != null && websocket.State == WebSocketState.Open)
+            {
+                var msg = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                await websocket.SendAsync(new ArraySegment<byte>(msg), WebSocketMessageType.Text, endOfMessage, CancellationToken.None);
+
+            }
+
+        }
     }
+
 }
