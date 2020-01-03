041b34aad2cca2a848465f91eed5fc9eddc5c173
Ataulhaye Ahmad <aahmad@daenet.com>
2020-01-03 17:21:33 +0100

    fixed bugs in neural model

--- NeoCortexApi/NeoCortexEntities/NeuroVisualizer/NeuroModel.cs
+++ NeoCortexApi/NeoCortexEntities/NeuroVisualizer/NeuroModel.cs
@@ -31,6 +31,11 @@ namespace NeoCortexEntities.NeuroVisualizer
 
             }
 
+            SynapseData synap = new SynapseData()
+            {
+                
+            };
+
         }
     }
     public class Settings
@@ -40,34 +45,28 @@ namespace NeoCortexEntities.NeuroVisualizer
 
     public class Area
     {
-        public MiniColumn[,] MiniColumnsBlock { get; set; }
+        public MiniColumn[,] MiniColumns { get; set; }//Till now two dimensinal matrix to save the mini colums
         public int AreaId { get; set; }
         public int Level { get; set; }
 
 
         public Area(int areaId, long[,] colDims)
         {
-            MiniColumnsBlock = new MiniColumn[colDims.GetLength(0), colDims.GetLength(1)];
-
+            MiniColumns = new MiniColumn[colDims.GetLength(0), colDims.GetLength(1)];
+            int NumCol = 0;
 
             double overlap = 0; // Where do I get overlap values
             for (int colDim0 = 0; colDim0 < colDims.GetLength(0); colDim0++)
             {
-               // MiniColumn[] row = new MiniColumn[colDims.GetLength(1)]; // a row contains MiniMini Columns
-                List<MiniColumn> row = new List<MiniColumn>(colDims.GetLength(1));
-
-                int colDim1;
-                for (colDim1 = 0; colDim1 < colDims.GetLength(1); colDim1++)
+                for (int colDim1 = 0; colDim1 < colDims.GetLength(1); colDim1++)
                 {
-                    //row[colDim0] = new MiniColumn(areaId, overlap, colDim0, colDim1);
-                    row.Insert(colDim1, new MiniColumn(areaId, overlap, colDim0, colDim1));
-
-                    // row[colDim0] = new MiniColumn(areaId, overlap, new long[colDim0, colDim1]);
-                    //Minicolumn(settings, areaId, [this.id, miniColDim0, miniColDim1], randomOverlap, (miniColDim0 + X), (miniColDim1 + Z)))
+               
+                    MiniColumns[colDim0, colDim1] = new MiniColumn(areaId, overlap, colDim0, colDim1);
+                    
                 }
 
-                MiniColumnsBlock[colDim0, colDim1-1] = row[row.Count-1];
             }
+            AreaId = areaId;
 
         }
     }
@@ -80,7 +79,6 @@ namespace NeoCortexEntities.NeuroVisualizer
 
 
         public MiniColumn(int areaId, double overlap, int colDim0, int colDim1)//areaId, overlap, colDim0, colDim1
-        //    public MiniColumn(int areaId, double overlap, long[,] columnDims)//areaId, overlap, colDim0, colDim1
         {
            
             AreaId = areaId;
@@ -95,11 +93,12 @@ namespace NeoCortexEntities.NeuroVisualizer
 
 
 
-            for (int numberOfCells = 0; numberOfCells < Cells.Length; numberOfCells++)
+            for (int i = 0; i < Cells.Length; i++)
             {
 
-                Cell cell = new Cell(areaId, parentColumnIndx, numberOfCells);
-                Cells[numberOfCells] = cell;
+                Cell cell = new Cell(areaId, parentColumnIndx, i);
+
+                Cells[i] = cell;
 
             }
             saveCells(Cells);
@@ -120,10 +119,8 @@ namespace NeoCortexEntities.NeuroVisualizer
     }
     public class SynapseData
     {
-        // public int AreaId { get; set; }
 
         public CellType SegmentCellType;
-
         public Synapse Synapse { get; set; }
         public Cell PreCell { get; set; }
         public Cell PostCell { get; set; }
