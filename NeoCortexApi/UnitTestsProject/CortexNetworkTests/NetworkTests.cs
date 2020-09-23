186b1c72bcbb31792fce7bba96c07b3d5f60c3dc
Damir Dobric <ddobric@daenet.com>
2020-09-23 13:06:38 +0200

    Increment net standard to 2.1 and Net Core 3.1

--- NeoCortexApi/UnitTestsProject/CortexNetworkTests/NetworkTests.cs
+++ NeoCortexApi/UnitTestsProject/CortexNetworkTests/NetworkTests.cs
@@ -464,7 +464,7 @@ namespace UnitTestsProject
             // List<double> inputValues = new List<double>(new double[] { 0.0, 1.0, 0.0, 2.
 
             // not stable with 2048 cols 25 cells per column and 0.02 * numColumns synapses on segment.
-            // Stable with permanence decrement 0.25/ increment 0.15:  30 repeats with 100% of accuracy. Elapsed time: 11 min.
+            // Stable with permanence decrement 0.25/ increment  Host Project mived to 3.10.15:  30 repeats with 100% of accuracy. Elapsed time: 11 min.
             // With increment=0.2 and decrement 0.3 has taken 15 min and didn't entered the stable state.
             List<double> inputValues = new List<double>(new double[] { 0.0, 1.0, 0.0, 2.0, 3.0, 4.0, 5.0, 6.0, 5.0, 4.0, 3.0, 7.0, 1.0, 9.0, 12.0, 11.0, 12.0, 13.0, 14.0, 11.0, 12.0, 14.0, 5.0, 7.0, 6.0, 9.0, 3.0, 4.0, 3.0, 4.0, 3.0, 4.0 });
 
