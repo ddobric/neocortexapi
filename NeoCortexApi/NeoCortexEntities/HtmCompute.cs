d7b2be9f88b2fb21892c6ba3ad1d191eb0b26b6a
Damir Dobric <ddobric@daenet.com>
2020-11-28 10:50:36 +0100

    Unneeded comments removed

--- NeoCortexApi/NeoCortexEntities/HtmCompute.cs
+++ NeoCortexApi/NeoCortexEntities/HtmCompute.cs
@@ -46,33 +46,7 @@ namespace NeoCortexApi
             return retVal;
         }
 
-        /**
-       * Maps a column to its input bits. This method encapsulates the topology of
-       * the region. It takes the index of the column as an argument and determines
-       * what are the indices of the input vector that are located within the
-       * column's potential pool. The return value is a list containing the indices
-       * of the input bits. The current implementation of the base class only
-       * supports a 1 dimensional topology of columns with a 1 dimensional topology
-       * of inputs. To extend this class to support 2-D topology you will need to
-       * override this method. Examples of the expected output of this method:
-       * * If the potentialRadius is greater than or equal to the entire input
-       *   space, (global visibility), then this method returns an array filled with
-       *   all the indices
-       * * If the topology is one dimensional, and the potentialRadius is 5, this
-       *   method will return an array containing 5 consecutive values centered on
-       *   the index of the column (wrapping around if necessary).
-       * * If the topology is two dimensional (not implemented), and the
-       *   potentialRadius is 5, the method should return an array containing 25
-       *   '1's, where the exact indices are to be determined by the mapping from
-       *   1-D index to 2-D position.
-       * 
-       * @param c             {@link Connections} the main memory model
-       * @param columnIndex   The index identifying a column in the permanence, potential
-       *                      and connectivity matrices.
-       * @param wrapAround    A boolean value indicating that boundaries should be
-       *                      ignored.
-       * @return
-       */
+      
         /// <summary>
         /// Maps a column to its input bits. This method encapsulates the topology of the region. It takes the index of the column as an argument and determines 
         /// what are the indices of the input vector that are located within the column's potential pool. The return value is a list containing the indices of 
@@ -125,25 +99,7 @@ namespace NeoCortexApi
             return sb.ToString();
         }
 
-        /**
-        * Uniform Column Mapping 
-        * Maps a column to its respective input index, keeping to the topology of
-        * the region. It takes the index of the column as an argument and determines
-        * what is the index of the flattened input vector that is to be the center of
-        * the column's potential pool. It distributes the columns over the inputs
-        * uniformly. The return value is an integer representing the index of the
-        * input bit. Examples of the expected output of this method:
-        * * If the topology is one dimensional, and the column index is 0, this
-        *   method will return the input index 0. If the column index is 1, and there
-        *   are 3 columns over 7 inputs, this method will return the input index 3.
-        * * If the topology is two dimensional, with column dimensions [3, 5] and
-        *   input dimensions [7, 11], and the column index is 3, the method
-        *   returns input index 8. 
-        *   
-        * @param columnIndex   The index identifying a column in the permanence, potential
-        *                      and connectivity matrices.
-        * @return              Flat index of mapped column.
-        */
+      
         /// <summary>
         /// Uniform Column Mapping <br></br>
         /// Maps a column to its respective input index, keeping to the topology of the region. It takes the index of the column as an argument and determines 
@@ -306,19 +262,7 @@ namespace NeoCortexApi
             return baseNum;
         }
 
-
-        /**
-       * Gets a neighborhood of inputs.
-       * 
-       * Simply calls topology.wrappingNeighborhood or topology.neighborhood.
-       * 
-       * A subclass can insert different topology behavior by overriding this method.
-       * 
-       * @param c                     the {@link Connections} memory encapsulation
-       * @param centerInput           The center of the neighborhood.
-       * @param potentialRadius       Span of the input field included in each neighborhood
-       * @return                      The input's in the neighborhood. (1D)
-       */
+              
         /// <summary>
         /// Gets a neighborhood of inputs. Simply calls topology.wrappingNeighborhood or topology.neighborhood. A subclass can insert different topology behavior by overriding this method.
         /// </summary>
@@ -335,24 +279,6 @@ namespace NeoCortexApi
         }
 
 
-        /**
-     * Initializes the permanences of a column. The method
-     * returns a 1-D array the size of the input, where each entry in the
-     * array represents the initial permanence value between the input bit
-     * at the particular index in the array, and the column represented by
-     * the 'index' parameter.
-     * 
-     * @param c                 the {@link Connections} which is the memory model
-     * @param potentialPool     
-     *                          
-     *                          
-     *                          
-     * @param index             the index of the column being initialized
-     * @param connectedPct      A value between 0 or 1 specifying the percent of the input
-     *                          bits that might maximally start off in a connected state.
-     *                          0.7 means, maximally 70% of potential might be connected
-     * @return
-     */
         /// <summary>
         /// Initializes the permanences of a column. The method returns a 1-D array the size of the input, where each entry in the array represents the initial 
         /// permanence value between the input bit at the particular index in the array, and the column represented by the 'index' parameter.
@@ -387,17 +313,7 @@ namespace NeoCortexApi
             return perm;
         }
 
-        /**
-      * Returns a randomly generated permanence value for a synapse that is
-      * initialized in a connected state. The basic idea here is to initialize
-      * permanence values very close to synPermConnected so that a small number of
-      * learning steps could make it disconnected or connected.
-      *
-      * Note: experimentation was done a long time ago on the best way to initialize
-      * permanence values, but the history for this particular scheme has been lost.
-      * 
-      * @return  a randomly generated permanence value
-      */
+      
         /// <summary>
         /// Returns a randomly generated permanence value for a synapse that is initialized in a connected state. The basic idea here is to initialize
         /// permanence values very close to <c>synPermConnected</c> so that a small number of learning steps could make it disconnected or connected.
@@ -521,28 +437,7 @@ namespace NeoCortexApi
                 ArrayUtils.RaiseValuesBy(htmConfig.SynPermBelowStimulusInc, perm);
             }
         }
-        /**
-        * This method updates the permanence matrix with a column's new permanence
-        * values. The column is identified by its index, which reflects the row in
-        * the matrix, and the permanence is given in 'sparse' form, i.e. an array
-        * whose members are associated with specific indexes. It is in
-        * charge of implementing 'clipping' - ensuring that the permanence values are
-        * always between 0 and 1 - and 'trimming' - enforcing sparseness by zeroing out
-        * all permanence values below 'synPermTrimThreshold'. It also maintains
-        * the consistency between 'permanences' (the matrix storing the
-        * permanence values), 'connectedSynapses', (the matrix storing the bits
-        * each column is connected to), and 'connectedCounts' (an array storing
-        * the number of input bits each column is connected to). Every method wishing
-        * to modify the permanence matrix should do so through this method.
-        * 
-        * @param c                 the {@link Connections} which is the memory model.
-        * @param perm              An array of permanence values for a column. The array is
-        *                          "dense", i.e. it contains an entry for each input bit, even
-        *                          if the permanence value is 0.
-        * @param column            The column in the permanence, potential and connectivity matrices
-        * @param maskPotential     The indexes of inputs in the specified {@link Column}'s pool.
-        * @param raisePerm         a boolean value indicating whether the permanence values
-        */
+      
         /// <summary>
         /// This method updates the permanence matrix with a column's new permanence values. The column is identified by its index, which reflects the row in
         /// the matrix, and the permanence is given in 'sparse' form, i.e. an array whose members are associated with specific indexes. It is in charge of 
