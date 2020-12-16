f418609ad38c85f41b23647606d63fdc997069df
QuangBui3101 <buinhatquang31011996@gmail.com>
2020-12-16 10:24:14 +0100

    add sample project

---
+++ NeoCortexApi/Tutorial/NeoCortexApiSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeoCortexApiSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
