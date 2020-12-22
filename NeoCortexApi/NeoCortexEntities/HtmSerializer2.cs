d32f88bf525d30d497499fd1871de88782abe46f
Damir Dobric <ddobric@daenet.com>
2020-12-22 16:02:19 +0100

    changes review

--- NeoCortexApi/NeoCortexEntities/HtmSerializer2.cs
+++ NeoCortexApi/NeoCortexEntities/HtmSerializer2.cs
@@ -11,9 +11,9 @@ using System.Text;
 
 namespace NeoCortexApi
 {
-    /**
-   * Contains methods for Serialization and Deserialization and is applicable to Spatial Pooler and Temoral Memory Class
-   */
+   /// <summary>
+   /// Serialization class used for serialization and deserialization of primitive types.
+   /// </summary>
     public class HtmSerializer2
     {
         //SP
@@ -21,19 +21,31 @@ namespace NeoCortexApi
 
         const string typeDelimiter = " ";
 
+        /// <summary>
+        /// Serializes the begin marker of the type.
+        /// </summary>
+        /// <param name="typeName"></param>
+        /// <param name="sw"></param>
         public void SerializeBegin(String typeName, StreamWriter sw)
         {
             //
             // -- BEGIN ---
             // typeName
 
+            sw.WriteLine();
             sw.Write($"{typeDelimiter} BEGIN '{typeName}' {typeDelimiter}");
             sw.WriteLine();
             
         }
 
+        /// <summary>
+        /// Serialize the end marker of the type.
+        /// </summary>
+        /// <param name="typeName"></param>
+        /// <param name="sw"></param>
         public void SerializeEnd(String typeName, StreamWriter sw)
         {
+            sw.WriteLine();
             sw.Write($"{typeDelimiter} END '{typeName}' {typeDelimiter}");
             sw.WriteLine();
         }
@@ -63,6 +75,11 @@ namespace NeoCortexApi
 
         }
 
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
         public int ReadIntValue(StreamReader reader)
         {
             throw new NotImplementedException();
