ef992a0a02cfacf5be659c801366ae4f4199de08
Damir Dobric <ddobric@daenet.com>
2020-12-27 14:41:30 +0100

    Working on TMMT

--- NeoCortexApi/NeoCortexApi/TemporalMemory.cs
+++ NeoCortexApi/NeoCortexApi/TemporalMemory.cs
@@ -9,6 +9,8 @@ using NeoCortexApi.Utility;
 using static NeoCortexApi.Entities.Connections;
 using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace NeoCortexApi
 {
@@ -127,6 +129,128 @@ namespace NeoCortexApi
                 ActivColumnIndicies = activeColumnIndices
             };
 
+            ConcurrentDictionary<int, ComputeCycle> cycles = new ConcurrentDictionary<int, ComputeCycle>();
+
+            ColumnData activeColumnData = new ColumnData();
+
+            ISet<Cell> prevActiveCells = conn.ActiveCells;
+            ISet<Cell> prevWinnerCells = conn.WinnerCells;
+
+            // The list of active columns.
+            List<Column> activeColumns = new List<Column>();
+
+            foreach (var indx in activeColumnIndices.OrderBy(i => i))
+            {
+                activeColumns.Add(conn.GetColumn(indx));
+            }
+
+            Func<Object, Column> segToCol = (segment) =>
+            {
+                var colIndx = ((DistalDendrite)segment).ParentCell.ParentColumnIndex;
+                var parentCol = this.connections.HtmConfig.Memory.GetColumn(colIndx);
+                return parentCol;
+            };
+
+            Func<object, Column> times1Fnc = x => (Column)x;
+
+            var list = new Pair<List<object>, Func<object, Column>>[3];
+            list[0] = new Pair<List<object>, Func<object, Column>>(Array.ConvertAll(activeColumns.ToArray(), item => (object)item).ToList(), times1Fnc);
+            list[1] = new Pair<List<object>, Func<object, Column>>(Array.ConvertAll(conn.ActiveSegments.ToArray(), item => (object)item).ToList(), segToCol);
+            list[2] = new Pair<List<object>, Func<object, Column>>(Array.ConvertAll(conn.MatchingSegments.ToArray(), item => (object)item).ToList(), segToCol);
+
+            GroupBy2<Column> grouper = GroupBy2<Column>.Of(list);
+
+            double permanenceIncrement = conn.HtmConfig.PermanenceIncrement;
+            double permanenceDecrement = conn.HtmConfig.PermanenceDecrement;
+
+            ParallelOptions opts = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = Environment.ProcessorCount
+            };
+
+            //
+            // Grouping by columns, which have active and matching segments.
+            Parallel.ForEach(grouper, opts, (tuple) =>
+            {
+                activeColumnData = activeColumnData.Set(tuple);
+
+                if (activeColumnData.IsExistAnyActiveCol(cIndexofACTIVE_COLUMNS))
+                {
+                    // If there are some active segments on the column already...
+                    if (activeColumnData.ActiveSegments != null && activeColumnData.ActiveSegments.Count > 0)
+                    {
+                        //Debug.Write(".");
+
+                        List<Cell> cellsOwnersOfActSegs = ActivatePredictedColumn(conn, activeColumnData.ActiveSegments,
+                            activeColumnData.MatchingSegments, prevActiveCells, prevWinnerCells,
+                                permanenceIncrement, permanenceDecrement, learn, cycle.ActiveSynapses);
+
+                        ComputeCycle colCycle = new ComputeCycle();
+                        cycles[tuple.Key.Index] = colCycle;
+
+                        foreach (var item in cellsOwnersOfActSegs)
+                        {
+                            colCycle.ActiveCells.Add(item);
+                            colCycle.WinnerCells.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        //
+                        // If no active segments are detected (start of learning) then all cells are activated
+                        // and a random single cell is chosen as a winner.
+                        BurstingResult burstingResult = BurstColumn(conn, activeColumnData.Column(), activeColumnData.MatchingSegments,
+                            prevActiveCells, prevWinnerCells, permanenceIncrement, permanenceDecrement, conn.HtmConfig.Random,
+                               learn);
+
+                        // DRAFT. Removing this as unnecessary.
+                        //cycle.ActiveCells.Add(burstingResult.BestCell);
+
+                        ComputeCycle colCycle = new ComputeCycle();
+                        cycles[tuple.Key.Index] = colCycle;
+
+                        //
+                        // Here we activate all cells by putting them to list of active cells.
+                        foreach (var item in burstingResult.Cells)
+                        {
+                            colCycle.ActiveCells.Add(item);
+                        }
+
+                        //var actSyns = conn.getReceptorSynapses(burstingResult.BestCell).Where(s=>prevActiveCells.Contains(s.SourceCell));
+                        //foreach (var syn in actSyns)
+                        //{
+                        //    cycle.ActiveSynapses.Add(syn);
+                        //}
+
+                        colCycle.WinnerCells.Add((Cell)burstingResult.BestCell);
+                    }
+                }
+                else
+                {
+                    if (learn)
+                    {
+                        PunishPredictedColumn(conn, activeColumnData.ActiveSegments, activeColumnData.MatchingSegments,
+                            prevActiveCells, prevWinnerCells, conn.HtmConfig.PredictedSegmentDecrement);
+                    }
+                }
+            });
+
+            foreach (var colCycle in cycles.Values)
+            {
+                cycle.ActiveCells.AddRange(colCycle.ActiveCells);
+                cycle.WinnerCells.AddRange(colCycle.WinnerCells);
+            }
+
+            return cycle;
+        }
+
+        protected ComputeCycle ActivateCellsOriginal(Connections conn, int[] activeColumnIndices, bool learn)
+        {
+            ComputeCycle cycle = new ComputeCycle
+            {
+                ActivColumnIndicies = activeColumnIndices
+            };
+
             ColumnData activeColumnData = new ColumnData();
 
             ISet<Cell> prevActiveCells = conn.ActiveCells;
@@ -170,7 +294,7 @@ namespace NeoCortexApi
                     // If there are some active segments on the column already...
                     if (activeColumnData.ActiveSegments != null && activeColumnData.ActiveSegments.Count > 0)
                     {
-                        Debug.Write(".");
+                        //Debug.Write(".");
 
                         List<Cell> cellsOwnersOfActSegs = ActivatePredictedColumn(conn, activeColumnData.ActiveSegments,
                             activeColumnData.MatchingSegments, prevActiveCells, prevWinnerCells,
@@ -475,7 +599,7 @@ namespace NeoCortexApi
             // If some matching segments exist, bursting will grab the segment with most potential synapses and adapt it.
             if (matchingSegments != null && matchingSegments.Count > 0)
             {
-                Debug.Write($"B.({matchingSegments.Count})");
+                // Debug.Write($"B.({matchingSegments.Count})");
 
                 DistalDendrite maxPotentialSeg = GetSegmentwithHighesPotential(conn, matchingSegments, prevActiveCells);
 
@@ -496,7 +620,7 @@ namespace NeoCortexApi
             }
             else
             {
-                Debug.Write("B.0");
+                // Debug.Write("B.0");
 
                 leastUsedOrMaxPotentialCell = this.GetLeastUsedCell(conn, cells, random);
                 if (learn)
