f8e6ecaf92d76a1b217590c6f46a3426052bf737
MounikaKolisetty <mouni.kolisetty@gmail.com>
2020-12-31 00:33:20 +0100

    Working on serialization

--- NeoCortexApi/UnitTestsProject/Similarity/SpatialPoolerSImilarityExperiments.cs
+++ NeoCortexApi/UnitTestsProject/Similarity/SpatialPoolerSImilarityExperiments.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
-using ImageBinarizer;
+using LeaningFoundation.ImageBinarizer;
 using System.Drawing;
 using NeoCortex;
 using NeoCortexApi.Network;
