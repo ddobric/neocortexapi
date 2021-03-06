ec65aae27f0377fa326e49f687903136db3162d4
MukitCSTE <mukitkhan07@gmail.com>
2021-03-06 18:12:20 +0000

    FeedForwardNetExperimentSample

---
+++ NeoCortexApi/FeedForwardNetExperiment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FeedForwardNetExperiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FeedForwardNetExperiment experiment = new FeedForwardNetExperiment();
+            experiment.FeedForwardNetTest();
+
+        }
+    }
+}
