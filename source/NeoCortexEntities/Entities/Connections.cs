fbba9fa8d56aced4e484f6554e5e4e92cdcfa109
Damir Dobric <ddobric@daenet.com>
2021-06-20 14:48:50 +0200

    LastActivity moved from Connectgions into TM

--- source/NeoCortexEntities/Entities/Connections.cs
+++ source/NeoCortexEntities/Entities/Connections.cs
@@ -208,10 +208,10 @@ namespace NeoCortexApi.Entities
 
         protected ConcurrentDictionary<int, DistalDendrite> m_SegmentForFlatIdx = new ConcurrentDictionary<int, DistalDendrite>();
 
-        /// <summary>
-        /// Stores each cycle's most recent activity
-        /// </summary>
-        public SegmentActivity LastActivity { get; set; }
+        ///// <summary>
+        ///// Stores each cycle's most recent activity
+        ///// </summary>
+        //public SegmentActivity LastActivity { get; set; }
 
         /// <summary>
         /// The segment creation number.
@@ -1930,7 +1930,7 @@ namespace NeoCortexApi.Entities
             // TODO!!!
             //ser.SerializeValue(this.m_SegmentForFlatIdx, writer);
 
-            this.LastActivity.Serialize(writer);
+            //this.LastActivity.Serialize(writer);
 
             ser.SerializeValue(this.NextSegmentOrdinal, writer);
             ser.SerializeValue(this.TieBreaker, writer);
