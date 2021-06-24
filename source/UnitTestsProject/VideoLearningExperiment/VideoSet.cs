3e3c7133171501fda62e2df51c992752bb1f91d0
Noath2302 <Noath2302@gmail.com>
2021-06-24 11:40:45 +0200

    Reading Videos instead of image TODO: make directory initiation in code relocate to VideoLearningExperiment/ instead of SequenceLearning
    DISCUSS: Moving Experiment to a seperate project outside UnitTestProject?

---
+++ source/UnitTestsProject/VideoLearningExperiment/VideoSet.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Diagnostics;
+using GleamTech.VideoUltimate;
+using NeoCortexApi.Utility;
+namespace UnitTestsProject.VideoLearningExperiments
+{
+    /// <summary>
+    /// <para>VideoSet is created to represent an folder of videos</para>
+    /// <para>Each videos will be read to a Video object</para>
+    /// <para>For Example:</para>
+    /// <para>
+    /// <br>A folder ball/ contains 3 file one.mp4, two.mp4 and three.mp4</br>
+    /// <br>will create a VideoSet with label "ball", which contains 3 Video objects:</br>
+    /// <br>each with name field respectively: one.mp4, two</br>
+    /// </para>
+    /// </summary>
+    public class VideoSet
+    {
+        public List<Video> videoEncodedList;
+        public List<string> videoName;
+        public string setLabel;
+        public VideoSet(string videoSetPath)
+        {
+            videoEncodedList = new();
+            videoName = new();
+
+            // Set the label of the video collection as the name of the folder that contains it 
+            this.setLabel = Path.GetFileNameWithoutExtension(videoSetPath);
+
+            // Read videos from the video folder path 
+            videoEncodedList = ReadVideos(videoSetPath);
+        }
+        /// <summary>
+        /// Read all videos within a provided folder's full path, the foleder name will be used as videoset's Label
+        /// </summary>
+        /// <param name="videoSetPath"> The Path of the folder that contains the videos</param>
+        private List<Video> ReadVideos(string videoSetPath)
+        {
+            List<Video> videoList = new();
+            // Iteate through each videos in the videos' folder
+            foreach (string file in Directory.GetFiles(videoSetPath))
+            {
+                string fileName = Path.GetFileName(videoSetPath);
+                videoName.Add(fileName);
+                Debug.WriteLine($"Video file name: {fileName}");
+                videoList.Add(new Video(file));
+            }
+            return videoList;
+        }
+    }
+    /// <summary>
+    /// <para>
+    /// <br>Represent a single video, which contains</br>
+    /// <br>Name of the video, including suffix format</br>
+    /// <br>List of int[], with each int[] is a frame in chronological order</br>
+    /// </para>
+    /// </summary>
+    public class Video
+    {
+        public string name;
+        public List<int[]> frames;
+        /// <summary>
+        /// Generate a Video object
+        /// </summary>
+        /// <param name="videoPath">full path to the video</param>
+        public Video(string videoPath)
+        {
+            this.frames = new();
+            this.name = videoPath;
+            this.frames = ReadVideo(videoPath);
+        }
+
+        private static List<int[]> ReadVideo(string videoPath)
+        {
+            List<int[]> frames_binaryArray = new();
+
+            // using VideosReader nuget package Videos.Ultimate from GleamTech
+            var videoFrameReader = new VideoFrameReader(videoPath);
+            int frameIndex = 0;
+            while (videoFrameReader.Read())
+            {
+                Debug.WriteLine("Coded Frame Number: " + videoFrameReader.CurrentFrameNumber);
+                Debug.WriteLine("Frame Index: " + frameIndex++);
+                Bitmap currentFrame = videoFrameReader.GetFrame();
+                int[] a = BitmapToBinaryArray(currentFrame, 10, 10);
+                Debug.WriteLine(a.ArrToString());
+                frames_binaryArray.Add(a);
+                Debug.WriteLine($"{frames_binaryArray[frameIndex-1]}");
+            }
+            //
+            return frames_binaryArray;
+        }
+
+        // Ultility Bitmap Image functions
+        /// <summary>
+        /// Bitmap Binary Encoder Binarize an Bitmap type image
+        /// and encode it to an int[] of binary value
+        /// <para>Example</para> 
+        /// <para>int[] inputBitArray = ImageToBin(imageBitmapType, scaledWidth = 10, scaledHeight = 10)</para>
+        /// <para>will produce an int[] inputBitArray with length 10 x 10 = 100 {1, 0, 0, 0, 1, 1, 1, ...}</para>
+        /// </summary>
+        /// <param name="file"> Bitmap image object to encode</param>
+        /// <param name="width"> Width of the output image before encoding</param>
+        /// <param name="height">Height of the output image before encoding</param>
+        /// <returns>returns an int[] of binarized pixels from Bitmap image object</returns>
+        private static int[] BitmapToBinaryArray(Bitmap file, int width, int height)
+        {
+            var image = new Bitmap(file);
+            Bitmap img = ResizeBitmap(image, width, height);
+            int length = img.Width * img.Height;
+            int[] imageBinary = new int[length];
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+                    if (pixel.R < 100)
+                    {
+                        imageBinary[j + i * img.Height] = 1;
+                    }
+                    else
+                    {
+                        imageBinary[j + i * img.Height] = 0;
+                    }
+                }
+            }
+            return imageBinary;
+        }
+        /// <summary>
+        /// Resize a Bitmap object to desired width and height
+        /// </summary>
+        /// <param name="bmp">Bitmap Object to be resized</param>
+        /// <param name="width">Output width</param>
+        /// <param name="height">Output height</param>
+        /// <returns></returns>
+        private static Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
+        {
+            Bitmap result = new(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(bmp, 0, 0, width, height);
+            }
+            return result;
+        }
+    }
+
+}
---
+++ source/UnitTestsProject/VideoLearningExperiment/VideosExperiment.cs
@@ -0,0 +1,299 @@
+﻿// Copyright (c) Damir Dobric. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NeoCortexApi.Network;
+using NeoCortexApi;
+using NeoCortexApi.Entities;
+using System.Diagnostics;
+using NeoCortexApi.Utility;
+using System.IO;
+using System.Threading;
+using NeoCortexApi.Classifiers;
+
+namespace UnitTestsProject.VideoLearningExperiments
+{
+    /// <summary>
+    /// This Experiment focus on learning and recognition of videos 
+    /// The videos are stored as short mp4 clip in TrainingVideos/ 
+    /// more doc/...
+    /// </summary>
+    [TestClass]
+    public class VideosExperiment
+    {
+        int[] inputBits = { 225 };
+        int[] numColumns = { 2048 };
+
+        [TestMethod]
+        [TestCategory("Experiment")]
+        [Timeout(TestTimeout.Infinite)]
+        public void VideosLearningExperiment()
+        {
+            // Input videos are stored in different folders under SequenceLearningExperiments/TrainingVideos/
+            string[] videoSetPaths = GetVideoSetPaths("VideoLearningExperiments");
+            List<VideoSet> videoData = new();
+
+            // with their folder's names as key value
+            foreach( string path in videoSetPaths)
+            {
+                videoData.Add(new VideoSet(path));
+            }
+            foreach (VideoSet set in videoData)
+            {
+                Debug.WriteLine($"VIDEO SET LABEL: {set.setLabel}");
+                foreach (Video vid in set.videoEncodedList)
+                {
+                    Debug.WriteLine($"  VIDEO NAME: {vid.name}");
+                    foreach (int[] frame in vid.frames)
+                    {
+                        Debug.WriteLine($"      Frame : {frame.ArrToString()}");
+                    }
+                }
+            }
+
+            //==================================== initiating the CLA HTM model ===================================
+            /*HtmConfig htm = getHTM();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            int maxMatchCnt = 0;
+            bool learn = true;
+
+            CortexNetwork net = new CortexNetwork("my cortex");
+            List<CortexRegion> regions = new List<CortexRegion>();
+            CortexRegion region0 = new CortexRegion("1st Region");
+
+            regions.Add(region0);
+
+            var mem = new Connections(htm);
+
+            bool isInStableState = false;
+
+            //HtmClassifier<double, ComputeCycle> cls = new HtmClassifier<double, ComputeCycle>();
+            HtmClassifier<string, ComputeCycle> cls = new HtmClassifier<string, ComputeCycle>();
+
+            TemporalMemory tm1 = new TemporalMemory();
+
+            HomeostaticPlasticityController hpa = new HomeostaticPlasticityController(mem, numInputs * 55, (isStable, numPatterns, actColAvg, seenInputs) =>
+            {
+                if (isStable)
+                    // Event should be fired when entering the stable state.
+                    Debug.WriteLine($"STABLE: Patterns: {numPatterns}, Inputs: {seenInputs}, iteration: {seenInputs / numPatterns}");
+                else
+                    // Ideal SP should never enter unstable state after stable state.
+                    Debug.WriteLine($"INSTABLE: Patterns: {numPatterns}, Inputs: {seenInputs}, iteration: {seenInputs / numPatterns}");
+
+                Assert.IsTrue(numPatterns == numInputs);
+                learn = isInStableState = isStable;
+                cls.ClearState();
+
+                tm1.Reset(mem);
+            }, numOfCyclesToWaitOnChange: 25);
+
+
+            SpatialPoolerMT sp1 = new SpatialPoolerMT(hpa);
+            sp1.Init(mem, UnitTestHelpers.GetMemory());
+            tm1.Init(mem);
+
+            CortexLayer<object, object> layer1 = new CortexLayer<object, object>("L1");
+            region0.AddLayer(layer1);
+            layer1.HtmModules.Add("sp", sp1);
+            layer1.HtmModules.Add("tm", tm1);
+
+            int cycle = 0;
+            int matches = 0;
+
+            string lastPredictedValue = "0";
+
+
+            int maxCycles = 4200;
+            int maxPrevInputs = tempInput.Count - 1;
+            List<string> previousInputs = new List<string>();
+            previousInputs.Add(tempInput[0].ArrToString());
+
+            // Training SP to get stable. New-born stage.
+            //
+            for (int i = 0; i < maxCycles; i++)
+            {
+                matches = 0;
+
+                cycle++;
+
+                Debug.WriteLine($"-------------- Newborn Cycle {cycle} ---------------");
+
+                for (int j = 0; j < tempInput.Count;j+=1)
+                {
+                    Debug.WriteLine($" -- {InputVideos[0].ImageNames[j]} -- {tempInput[j].ArrToString()} --");
+
+                    var lyrOut = sp1.Compute(tempInput[j], learn);
+                    Debug.WriteLine($"SDR:  {lyrOut.ArrToString()}");
+                    if (isInStableState)
+                        break;
+                }
+
+                if (isInStableState)
+                    break;
+            }
+
+
+            //
+            // Now training with SP+TM. SP is pretrained on the given input pattern set.
+            for (int i = 0; i < maxCycles; i++)
+            {
+                matches = 0;
+
+                cycle++;
+
+                Debug.WriteLine($"-------------- Cycle {cycle} ---------------");
+
+                for (int j = 0; j < tempInput.Count;j+=1)
+                {
+                    Debug.WriteLine($"-------------- {InputVideos[0].ImageNames[j]} ---------------");
+
+                    var lyrOut = layer1.Compute(tempInput[j], learn) as ComputeCycle;
+
+                    // lyrOut is null when the TM is added to the layer inside of HPC callback by entering of the stable state.
+                    //if (isInStableState && lyrOut != null)
+                    {
+                        var activeColumns = layer1.GetResult("sp") as int[];
+
+                        //layer2.Compute(lyrOut.WinnerCells, true);
+                        //activeColumnsLst[input].Add(activeColumns.ToList());
+
+                        previousInputs.Add(InputVideos[0].ImageNames[j]);
+                        if (previousInputs.Count > (maxPrevInputs + 1))
+                            previousInputs.RemoveAt(0);
+
+                        // In the pretrained SP with HPC, the TM will quickly learn cells for patterns
+                        // In that case the starting sequence 4-5-6 might have the sam SDR as 1-2-3-4-5-6,
+                        // Which will result in returning of 4-5-6 instead of 1-2-3-4-5-6.
+                        // HtmClassifier allways return the first matching sequence. Because 4-5-6 will be as first
+                        // memorized, it will match as the first one.
+                        if (previousInputs.Count < maxPrevInputs)
+                            continue;
+
+                        string key = GetKey(previousInputs, InputVideos[0].ImageNames[j]);
+
+                        List<Cell> actCells;
+
+                        if (lyrOut.ActiveCells.Count == lyrOut.WinnerCells.Count)
+                        {
+                            actCells = lyrOut.ActiveCells;
+                        }
+                        else
+                        {
+                            actCells = lyrOut.WinnerCells;
+                        }
+
+                        cls.Learn(key, actCells.ToArray());
+
+                        if (learn == false)
+                            Debug.WriteLine($"Inference mode");
+
+                        Debug.WriteLine($"Col  SDR: {Helpers.StringifyVector(lyrOut.ActivColumnIndicies)}");
+                        Debug.WriteLine($"Cell SDR: {Helpers.StringifyVector(actCells.Select(c => c.Index).ToArray())}");
+
+                        if (key == lastPredictedValue)
+                        {
+                            matches++;
+                            Debug.WriteLine($"Match. Actual value: {key} - Predicted value: {lastPredictedValue}");
+                        }
+                        else
+                            Debug.WriteLine($"Missmatch! Actual value: {key} - Predicted value: {lastPredictedValue}");
+
+                        if (lyrOut.PredictiveCells.Count > 0)
+                        {
+                            var predictedInputValue = cls.GetPredictedInputValue(lyrOut.PredictiveCells.ToArray());
+
+                            Debug.WriteLine($"Current Input: {InputVideos[0].ImageNames[j]} \t| Predicted Input: {predictedInputValue}");
+
+                            lastPredictedValue = predictedInputValue;
+                        }
+                        else
+                        {
+                            Debug.WriteLine($"NO CELLS PREDICTED for next cycle.");
+                            lastPredictedValue = String.Empty;
+                        }
+                    }
+                }
+
+                // The brain does not do that this way, so we don't use it.
+                // tm1.reset(mem);
+
+                double accuracy = (double)matches / (double)tempInput.Count * 100.0;
+
+                Debug.WriteLine($"Cycle: {cycle}\tMatches={matches} of {tempInput.Count}\t {accuracy}%");
+
+                if (accuracy == 100.0)
+                {
+                    maxMatchCnt++;
+                    Debug.WriteLine($"100% accuracy reched {maxMatchCnt} times.");
+                    //
+                    // Experiment is completed if we are 30 cycles long at the 100% accuracy.
+                    if (maxMatchCnt >= 30)
+                    {
+                        sw.Stop();
+                        Debug.WriteLine($"Exit experiment in the stable state after 30 repeats with 100% of accuracy. Elapsed time: {sw.ElapsedMilliseconds / 1000 / 60} min.");
+                        learn = false;
+                        break;
+                    }
+                }
+                else if (maxMatchCnt > 0)
+                {
+                    Debug.WriteLine($"At 100% accuracy after {maxMatchCnt} repeats we get a drop of accuracy with {accuracy}. This indicates instable state. Learning will be continued.");
+                    maxMatchCnt = 0;
+                }
+            }
+
+            Debug.WriteLine("------------ END ------------");
+            */
+        }
+
+        private HtmConfig getHTM()
+        {
+            HtmConfig htm = new HtmConfig(inputBits, numColumns);
+            htm.SetHtmConfigDefaultParameters(inputBits, numColumns);
+            htm.NumActiveColumnsPerInhArea = (int)(0.02 * numColumns[0]);
+            htm.MaxSynapsesPerSegment = (int)(0.02 * numColumns[0]);
+            return htm;
+        }
+
+        
+        private static string GetKey(List<string> prevInputs, string input)
+        {
+            string key = String.Empty;
+
+            for (int i = 0; i < prevInputs.Count; i++)
+            {
+                if (i > 0)
+                    key += "-";
+
+                key += (prevInputs[i]);
+            }
+
+            return key;
+        }
+
+        /// <summary>
+        /// <para>Get Folders under bin/experimentOutputFolder/TrainingVideos</para>
+        /// <para>Project specific function for getting all the folders that conatains videos</para>
+        /// </summary>
+        /// <param name="experimentOutputFolder">Output folder name of the test, found under Debug/bin/</param>
+        /// <returns></returns>
+        private static string[] GetVideoSetPaths(string experimentOutputFolder)
+        {
+            string currentDir = Directory.GetCurrentDirectory();
+
+            // Get the root path of training videos.
+            string testDir = $"{currentDir}\\{experimentOutputFolder}\\TrainingVideos";
+
+            // Get all the folders that contain video sets under TrainingVideos/
+            string[] videoSetPaths = Directory.GetDirectories(testDir, "*", SearchOption.TopDirectoryOnly);
+
+            // Return an array of video sets paths
+            return videoSetPaths;
+        }
+    }
+}
