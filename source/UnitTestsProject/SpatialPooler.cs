0d63ecddda87e0cb4af79206eb7e12e1b3ae5b57
Muhammad Jasim <muhammad.suhail@stud.fra-uas.de>
2022-02-06 20:59:35 +0100

    addition to SP

--- source/UnitTestsProject/SpatialPooler.cs
+++ source/UnitTestsProject/SpatialPooler.cs
@@ -1,24 +1,68 @@
-﻿using System;
+﻿using NeoCortexApi.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoCortexApi;
+using NeoCortexEntities.NeuroVisualizer;
 
-namespace HTM_SP_tm
+namespace UnitTestsProject
 {
-    internal class SpatialPooler
+    
+    
+    public class SpatialPooler
 
-    {   // Creates an empty pooler.
+    {
+        public static Parameters GetDefaultParams()
+        {
+            Random rnd = new Random(42);
+
+            var parameters = Parameters.getAllDefaultParameters();
+            parameters.Set(KEY.POTENTIAL_RADIUS, 10);
+            parameters.Set(KEY.POTENTIAL_PCT, 0.75);
+            parameters.Set(KEY.GLOBAL_INHIBITION, false);
+            parameters.Set(KEY.LOCAL_AREA_DENSITY, -1.0);
+            parameters.Set(KEY.NUM_ACTIVE_COLUMNS_PER_INH_AREA, 80.0);
+            parameters.Set(KEY.STIMULUS_THRESHOLD, 0);
+            parameters.Set(KEY.SYN_PERM_INACTIVE_DEC, 0.01);
+            parameters.Set(KEY.SYN_PERM_ACTIVE_INC, 0.1);
+            parameters.Set(KEY.SYN_PERM_CONNECTED, 0.1);
+            parameters.Set(KEY.MIN_PCT_OVERLAP_DUTY_CYCLES, 0.001);
+            parameters.Set(KEY.MIN_PCT_ACTIVE_DUTY_CYCLES, 0.001);
+            parameters.Set(KEY.WRAP_AROUND, true);
+            parameters.Set(KEY.DUTY_CYCLE_PERIOD, 10);
+            parameters.Set(KEY.MAX_BOOST, 1.0);
+            parameters.Set(KEY.RANDOM, rnd);
+
+            return parameters;
+        }
+
+        // Creates an empty pooler.
         SpatialPooler sp = new SpatialPooler();
 
         // Creates the cell-space
         Connections mem = new Connections();
+        private HomeostaticPlasticityController homeostaticPlasticityActivator;
 
-        // Initialize cell-space with set of required parameters.
-        parameters.apply(mem);
+        
 
-        // Initializes the SpatialPooler.
-        sp.init(mem);
+        public SpatialPooler()
+        {
+        }
+
+        public SpatialPooler(HomeostaticPlasticityController homeostaticPlasticityActivator)
+        {
+            this.homeostaticPlasticityActivator = homeostaticPlasticityActivator;
+        }
+
+        internal void Serialize(StreamWriter sw)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 
 } 
