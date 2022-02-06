a26225abb6e6e46c49921b5157f181cfb6719886
Muhammad Jasim <muhammad.suhail@stud.fra-uas.de>
2022-02-06 20:53:24 +0100

    connection init

--- source/UnitTestsProject/Connections.cs
+++ source/UnitTestsProject/Connections.cs
@@ -1,6 +1,31 @@
-﻿namespace HTM_SP_tm
+﻿using NeoCortexApi.Entities;
+using System;
+using System.IO;
+
+namespace UnitTestsProject
 {
     internal class Connections
     {
+        internal object ActiveSegments;
+        private HtmConfig matrix;
+
+        public Connections(HtmConfig matrix)
+        {
+            this.matrix = matrix;
+        }
+
+        public Connections()
+        {
+        }
+
+        internal void Serialize(StreamWriter sw)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Connections Deserialize(StreamReader sr)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
