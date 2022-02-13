3273f04f7bee8e7da57926f2f70e9f23a3ba4e51
Muhammad Jasim <muhammad.suhail@stud.fra-uas.de>
2022-02-13 16:33:59 +0100

    Initiate Deserializer

---
+++ source/UnitTestsProject/HTMSerialization_Deserialization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoCortexApi;
+using NeoCortexApi.Entities;
+using NeoCortexEntities.NeuroVisualizer;
+
+
+namespace UnitTestsProject
+{
+    [TestClass]
+    public class HTMSerialization_Deserialization
+    {
+        [TestMethod]
+        [TestCategory("DeSerialization")]
+
+    }
+
+}
