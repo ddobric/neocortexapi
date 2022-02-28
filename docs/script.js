86a7b61813cbe93db2e60694f7179677f97dc537
QuangBui3101 <buinhatquang31011996@gmail.com>
2022-02-28 02:33:27 +0100

    update inline script

--- docs/script.js
+++ docs/script.js
@@ -1,22 +1,6 @@
-// var btnGs = document.getElementById("get-started").addEventListener("click", function() {
-//     var element = document.getElementById("gs");
-//     element.scrollIntoView({behavior: 'smooth'});
-// });
-// var btnSp = document.getElementById("spatial-pooler").addEventListener("click", function() {
-//     var element = document.getElementById("sp");
-//     element.scrollIntoView({behavior: 'smooth'});
-// });
-// var btnGs = document.getElementById("temporal-memory").addEventListener("click", function() {
-//     var element = document.getElementById("tm");
-//     element.scrollIntoView({behavior: 'smooth'});
-// });
-
 scrollBtn("get-started", "gs");
 scrollBtn("spatial-pooler", "sp");
 scrollBtn("temporal-memory", "tm");
-
-// var btnGS = document.getElementById("spatial-pooler");
-// btnGS.click()
 
 function scrollBtn(btnId, targetId) {
   var btn = document.getElementById(btnId);
@@ -30,17 +14,16 @@ function addImageLink(x) {
   var imageHolders = document.getElementsByClassName("img-holder");
   if (x.matches) {
     for (var i = 0; i < imageHolders.length; i++) {
-        var holder = imageHolders[i];
-        var link = holder.getElementsByTagName("a");
-        if (link != null){
-            for (let index = 0; index < link.length; index++) {
-                const element = link[index];
-                holder.removeChild(element);
-            }
+      var holder = imageHolders[i];
+      var link = holder.getElementsByTagName("a");
+      if (link != null) {
+        for (let index = 0; index < link.length; index++) {
+          const element = link[index];
+          holder.removeChild(element);
         }
+      }
     }
   } else {
-      console.log("hello")
     for (var i = 0; i < imageHolders.length; i++) {
       var holder = imageHolders[i];
       var image = holder.getElementsByClassName("image")[0];
@@ -48,7 +31,9 @@ function addImageLink(x) {
         var link = document.createElement("a");
         link.href = image.src;
         link.target = "_blank";
-        link.appendChild(document.createTextNode("Click to view original size."));
+        link.appendChild(
+          document.createTextNode("Click to view original size.")
+        );
         holder.appendChild(link);
       }
     }
