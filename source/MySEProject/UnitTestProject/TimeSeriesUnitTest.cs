1a56a7310360cefabc67e2a1bcff4e2f995170d3
Md Rizwanul Islam <riad.rizwanul@gmail.com>
2022-03-25 19:51:37 +0600

    Added unit test project

---
+++ source/MySEProject/UnitTestProject/TimeSeriesUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TimeSeriesUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
