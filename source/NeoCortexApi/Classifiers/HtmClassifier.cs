ea66dd6ea7c7c6e507269d9b91b8444527dfb24d
Noath2302 <noath2302@gmail.com>
2022-07-04 11:16:20 +0200

    adding HtmClassifier with seperated Learn/GetPredictedInputValues function

--- source/NeoCortexApi/Classifiers/HtmClassifier.cs
+++ source/NeoCortexApi/Classifiers/HtmClassifier.cs
@@ -116,6 +116,11 @@ namespace NeoCortexApi.Classifiers
         {
             var cellIndicies = GetCellIndicies(output);
 
+            Learn(input, cellIndicies);
+        }
+
+        public void Learn(TIN input, int[] cellIndicies)
+        {
             if (m_AllInputs.ContainsKey(input) == false)
                 m_AllInputs.Add(input, new List<int[]>());
 
@@ -146,7 +151,12 @@ namespace NeoCortexApi.Classifiers
             }
         }
 
+        public List<ClassifierResult<TIN>> GetPredictedInputValues(Cell[] predictiveCells, short howMany = 1)
+        {
+            var cellIndicies = GetCellIndicies(predictiveCells);
 
+            return GetPredictedInputValues(cellIndicies, howMany);
+        }
 
         /// <summary>
         /// Gets multiple predicted values.
@@ -154,7 +164,7 @@ namespace NeoCortexApi.Classifiers
         /// <param name="predictiveCells">The current set of predictive cells.</param>
         /// <param name="howMany">The number of predections to return.</param>
         /// <returns>List of predicted values with their similarities.</returns>
-        public List<ClassifierResult<TIN>> GetPredictedInputValues(Cell[] predictiveCells, short howMany = 1)
+        public List<ClassifierResult<TIN>> GetPredictedInputValues(int[] cellIndicies, short howMany = 1)
         {
             List<ClassifierResult<TIN>> res = new List<ClassifierResult<TIN>>();
             double maxSameBits = 0;
@@ -162,15 +172,13 @@ namespace NeoCortexApi.Classifiers
             Dictionary<TIN, ClassifierResult<TIN>> dict = new Dictionary<TIN, ClassifierResult<TIN>>();
 
             var predictedList = new List<KeyValuePair<double, string>>();
-            if (predictiveCells.Length != 0)
+            if (cellIndicies.Length != 0)
             {
                 int indxOfMatchingInp = 0;
-                Debug.WriteLine($"Item length: {predictiveCells.Length}\t Items: {this.m_AllInputs.Keys.Count}");
+                Debug.WriteLine($"Item length: {cellIndicies.Length}\t Items: {this.m_AllInputs.Keys.Count}");
                 int n = 0;
 
                 List<int> sortedMatches = new List<int>();
-
-                var cellIndicies = GetCellIndicies(predictiveCells);
 
                 Debug.WriteLine($"Predictive cells: {cellIndicies.Length} \t {Helpers.StringifyVector(cellIndicies)}");
 
@@ -335,14 +343,13 @@ namespace NeoCortexApi.Classifiers
         /// <summary>
         /// Traces out all cell indicies grouped by input value.
         /// </summary>
-        public string TraceState(string fileName = null)
+        public void TraceState(string fileName = null)
         {
-            StringWriter strSw = new StringWriter();
-
             StreamWriter sw = null;
-
             if (fileName != null)
                 sw = new StreamWriter(fileName);
+            else
+                sw = new StreamWriter(fileName.Replace(".csv", "HtmClassifier.state.csv"));
 
             List<TIN> processedValues = new List<TIN>();
 
@@ -350,37 +357,36 @@ namespace NeoCortexApi.Classifiers
             // Trace out the last stored state.
             foreach (var item in this.m_AllInputs)
             {
-                strSw.WriteLine("");
-                strSw.WriteLine($"{item.Key}");
-                strSw.WriteLine($"{Helpers.StringifyVector(item.Value.Last())}");
-            }
-
-            strSw.WriteLine("........... Cell State .............");
-
-            foreach (var item in m_AllInputs)
-            {
-                strSw.WriteLine("");
-
-                strSw.WriteLine($"{item.Key}");
-
-                strSw.Write(Helpers.StringifySdr(new List<int[]> (item.Value)));
-
-                //foreach (var cellState in item.Value)
-                //{
-                //    var str = Helpers.StringifySdr(cellState);
-                //    strSw.WriteLine(str);
-                //}
+                Debug.WriteLine("");
+                Debug.WriteLine($"{item.Key}");
+                Debug.WriteLine($"{Helpers.StringifyVector(item.Value.Last())}");
             }
 
             if (sw != null)
             {
-                sw.Write(strSw.ToString());
                 sw.Flush();
                 sw.Close();
             }
 
-            Debug.WriteLine(strSw.ToString());
-            return strSw.ToString();
+            Debug.WriteLine("........... Cell State .............");
+
+            using (var cellStateSw = new StreamWriter(fileName.Replace(".csv", "HtmClassifier.fullstate.csv")))
+            {
+                foreach (var item in m_AllInputs)
+                {
+                    Debug.WriteLine("");
+                    Debug.WriteLine($"{item.Key}");
+
+                    cellStateSw.WriteLine("");
+                    cellStateSw.WriteLine($"{item.Key}");
+                    foreach (var cellState in item.Value)
+                    {
+                        var str = Helpers.StringifyVector(cellState);
+                        Debug.WriteLine(str);
+                        cellStateSw.WriteLine(str);
+                    }
+                }
+            }
         }
 
 
