f6d66a784f7d3628b8212d5419418d526df88938
सबिन (Sabin) <sabin.bajracharya@gmail.com>
2022-09-05 02:20:12 +0200

    Add: Find Active cell (spike) that is compatible with NeocortexApi cell

--- source/GridCell/Program.cs
+++ source/GridCell/Program.cs
@@ -1,13 +1,16 @@
-﻿using gridcells;
+﻿using GridCell;
 
 var spatial = new SpatialNavigation();
-spatial.RandomNavigation(3);
+spatial.RandomNavigation(10);
 spatial.Plot();
 
 
 var grid = new Grid();
 var simulation = new Simulation(grid, spatial.txx, spatial.tyy);
 simulation.run();
+
+var spike = new Spikes(simulation.GridCellsLog);
+spike.run(5);
 
 Console.WriteLine("End");
 
