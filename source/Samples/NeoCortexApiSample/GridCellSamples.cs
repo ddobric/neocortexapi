df6ff394de6a47797c8492e89f8ceec859157e35
सबिन (Sabin) <sabin.bajracharya@gmail.com>
2022-10-29 21:16:35 +0200

    Add Gridcell encoder - Encodes given x,y coordinate to SDR using Grid Cell concept

--- source/Samples/NeoCortexApiSample/GridCellSamples.cs
+++ source/Samples/NeoCortexApiSample/GridCellSamples.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GridCell;
+using GridCell.encoder;
 using GridCell.js;
+using NeoCortexApi;
 
 namespace NeoCortexApiSample
 {
@@ -26,13 +28,30 @@ namespace NeoCortexApiSample
 
         public void Run()
         {
-            int arenaSize = 100;
+            //int arenaSize = 100;
 
-            var gridConfig = new GConfig(arenaSize);
-            var grid = new GCell(gridConfig);
+            //var gridConfig = new GConfig(arenaSize);
+            //var grid = new GCell(gridConfig);
 
-            var simulation = new GSimulation(grid);
-            simulation.RunContinuous();
+            //var simulation = new GSimulation(grid);
+            //simulation.RunContinuous();
+
+            var gc = new GridCellEncoder(100);
+
+            var coordinates = new double[,] {
+                {1, 1},
+                {5, 6},
+                {10, 10},
+                {50, 50}
+            };
+
+            for (int i = 0; i < coordinates.GetLength(0); i++)
+            {
+                var activatedCells = gc.Encode(coordinates[i,0], coordinates[i,1]);
+
+                Console.WriteLine(Helpers.StringifyVector(activatedCells.ToArray()));
+            }
+
         }
     }
 }
