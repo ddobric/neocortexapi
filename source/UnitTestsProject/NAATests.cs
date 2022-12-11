420e0ac1db2c5949f0ae7937c1be531e287bf719
Damir Dobric <ddobric@daenet.de>
2022-12-11 10:56:57 +0100

    work on NAA and persistence

---
+++ source/UnitTestsProject/NAATests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoCortexApi;
+using NeoCortexApi.Entities;
+using System.Net.Http.Headers;
+
+namespace UnitTestsProject
+{
+    [TestClass]
+    public class NAATests
+    {
+        private HtmConfig GetHtmConfig(int inpBits, int columns)
+        {
+            var htmConfig = new HtmConfig(new int[] { inpBits }, new int[] { columns })
+            {
+                PotentialRadius = 5,
+                PotentialPct = 0.5,
+                GlobalInhibition = false,
+                LocalAreaDensity = -1.0,
+                NumActiveColumnsPerInhArea = 3.0,
+                StimulusThreshold = 0.0,
+                SynPermInactiveDec = 0.01,
+                SynPermActiveInc = 0.1,
+                SynPermConnected = 0.1,
+                MinPctOverlapDutyCycles = 0.1,
+                MinPctActiveDutyCycles = 0.1,
+                DutyCyclePeriod = 10,
+                MaxBoost = 10,
+                RandomGenSeed = 42,
+                Random = new ThreadSafeRandom(42),
+            };
+
+            return htmConfig;
+        }
+
+        [TestMethod]
+        [TestCategory("NAA")]
+        public void CreateAreaTest()
+        {
+            var cfg = GetHtmConfig(100, 1024);
+
+            CorticalArea caX = new CorticalArea("X", cfg);
+
+            Assert.IsTrue(1024 == caX.Columns.Count);
+
+            Assert.IsTrue(caX.AllCells.Count == 1024 * cfg.CellsPerColumn);
+
+        }
+    }
+}
