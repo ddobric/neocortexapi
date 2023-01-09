5cd1b89f77d766b845f6258e06cf058eb122e8ca
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2023-01-09 15:03:13 +0100

    Update Equal method tests for Column

--- source/UnitTestsProject/ColumnTests.cs
+++ source/UnitTestsProject/ColumnTests.cs
@@ -20,11 +20,28 @@ namespace UnitTestsProject
         [TestMethod]
         public void CompareColumn()
         {
-            HtmConfig config = new HtmConfig(new int[] { 5 }, new int[] { 5 });
+            HtmConfig config = new HtmConfig(new int[] { 5 }, new int[] { 5 }) { NumInputs = 1 };
+            HtmConfig config2 = new HtmConfig(new int[] { 5 }, new int[] { 5 }) { NumInputs = 2 };
 
+            //Test with different column index
             Column column1 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
             Column column2 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
             Column column3 = new Column(config.CellsPerColumn, 2, config.SynPermConnected, config.NumInputs);
+
+            //Test with different PotentialPool
+            Column column4 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
+            column4.CreatePotentialPool(config, new int[] { 1, 2, 3 }, -1);
+
+            Column column5 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
+            column5.CreatePotentialPool(config, new int[] { 1, 2, 3 }, -1);
+
+            Column column6 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
+            column6.CreatePotentialPool(config, new int[] { 0, 2, 2 }, -1);
+
+            //Test with different number of input neorn cells
+            Column column7 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
+            Column column8 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
+            Column column9 = new Column(config2.CellsPerColumn, 1, config2.SynPermConnected, config2.NumInputs);
 
             //Not same by reference
             Assert.IsFalse(column1 == column2);
@@ -35,6 +52,19 @@ namespace UnitTestsProject
 
             //column1 and column3 are NOT same by value (different column index)
             Assert.IsFalse(column1.Equals(column3));
+
+            //column4 and column5 are same by value 
+            Assert.IsTrue(column4.Equals(column5));
+
+            //column4 and column6 are NOT same by value (different Potential Pool)
+            Assert.IsFalse(column4.Equals(column6));
+
+            //column7 and column8 are same by value 
+            Assert.IsTrue(column7.Equals(column8));
+
+            //column7 and column9 are NOT same by value (different config)
+            Assert.IsFalse(column7.Equals(column9));
+
 
         }
     }
