de9285728508e598bc4a5c0ecafec15fe9c86968
Faiz <faizmohammedkhan7@gmail.com>
2023-01-21 14:10:45 +0100

    New MultiSequenceLearningProject class

---
+++ source/Samples/NeoCortexApiSample/MultiSequenceLearningProject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoCortexApi;
+using NeoCortexApi.Classifiers;
+using NeoCortexApi.Encoders;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Network;
+
+namespace NeoCortexApiSample
+{
+    public class MultiSequenceLearningProject
+    {
+        /// <summary>
+        /// Runs the learning of sequences.
+        /// </summary>
+        /// <param name="sequences">Dictionary of sequences. KEY is the sewuence name, the VALUE is th elist of element of the sequence.</param>
+        public Predictor RunExperiment(int inputBits, HtmConfig cfg, EncoderBase encoder, Dictionary<string, List<double>> sequences)
+        {
+           
+        }
+    }
+}
