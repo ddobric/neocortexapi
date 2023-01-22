c7fe2ed1897e445ebbb7a7494075a44cfdb3c7bf
Anurag <anurag.de@stud.fra-uas.de>
2023-01-22 23:10:56 +0100

    Testing RunMultiSimpleSequenceLearningExperiment

---
+++ mySEProject/anomaly_detection/anomaly_detection/Program.cs
@@ -0,0 +1,51 @@
+﻿using NeoCortexApi;
+using NeoCortexApi.Encoders;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using static NeoCortexApiSample.MultiSequenceLearning;
+
+namespace NeoCortexApiSample
+{
+    class AnomalyDetectionTest
+    {
+        /// <summary>
+        /// This sample shows a typical experiment code for SP and TM.
+        /// You must start this code in debugger to follow the trace.
+        /// and TM.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //
+            // Starts experiment that demonstrates how to learn spatial patterns.
+            //SpatialPatternLearning experiment = new SpatialPatternLearning();
+            //experiment.Run();
+
+            //
+            // Starts experiment that demonstrates how to learn spatial patterns.
+            //SequenceLearning experiment = new SequenceLearning();
+            //experiment.Run();
+
+            RunMultiSimpleSequenceLearningExperiment();
+
+        }
+
+        private static void RunMultiSimpleSequenceLearningExperiment()
+        {
+            Dictionary<string, List<double>> sequences = new Dictionary<string, List<double>>();
+
+            sequences.Add("S1", new List<double>(new double[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, }));
+            sequences.Add("S2", new List<double>(new double[] { 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0 }));
+
+            //
+            // Prototype for building the prediction engine.
+            MultiSequenceLearning experiment = new MultiSequenceLearning();
+            var predictor = experiment.Run(sequences);
+        }
+
+
+
+    }
+}
