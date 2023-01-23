b6293683a3829a6f0a6a367cb1e5576b8d53750a
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2023-01-23 23:02:30 +0100

    Update ColumnTests

--- source/UnitTestsProject/ColumnTests.cs
+++ source/UnitTestsProject/ColumnTests.cs
@@ -15,7 +15,6 @@ namespace UnitTestsProject
     {
         /// <summary>
         /// Column Equal method tests.
-        /// TODO: Compare ProximalDendrite and ConnectedInputCounterMatrix
         /// </summary>
         [TestMethod]
         public void CompareColumn()
@@ -33,25 +32,11 @@ namespace UnitTestsProject
             };
 
 
-            //Test with different column index
+            #region Test with different column index     
+            //Test with different column index 
             Column column1 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
             Column column2 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
             Column column3 = new Column(config.CellsPerColumn, 2, config.SynPermConnected, config.NumInputs);
-
-            //Test with different PotentialPool
-            Column column4 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
-            column4.CreatePotentialPool(config, new int[] { 1, 2, 3 }, -1);
-
-            Column column5 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
-            column5.CreatePotentialPool(config, new int[] { 1, 2, 3 }, -1);
-
-            Column column6 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
-            column6.CreatePotentialPool(config, new int[] { 0, 2, 2 }, -1);
-
-            //Test with different number of input neorn cells
-            Column column7 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
-            Column column8 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
-            Column column9 = new Column(config2.CellsPerColumn, 1, config2.SynPermConnected, config2.NumInputs);
 
             //Not same by reference
             Assert.IsFalse(column1 == column2);
@@ -63,17 +48,42 @@ namespace UnitTestsProject
             //column1 and column3 are NOT same by value (different column index)
             Assert.IsFalse(column1.Equals(column3));
 
+            #endregion
+
+            #region Test with different PotentialPool
+            //Test with different PotentialPool
+            Column column4 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
+            column4.CreatePotentialPool(config, new int[] { 1, 2, 3 }, -1);
+
+            Column column5 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
+            column5.CreatePotentialPool(config, new int[] { 1, 2, 3 }, -1);
+
+            Column column6 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
+            column6.CreatePotentialPool(config, new int[] { 0, 2, 2 }, -1);
+
             //column4 and column5 are same by value 
             Assert.IsTrue(column4.Equals(column5));
 
             //column4 and column6 are NOT same by value (different Potential Pool)
             Assert.IsFalse(column4.Equals(column6));
+            #endregion
+
+            #region Test with different connectedInputCounter 
+            //Test with different connectedInputCounter 
+            Column column7 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
+            Column column8 = new Column(config.CellsPerColumn, 1, config.SynPermConnected, config.NumInputs);
+            Column column9 = new Column(config2.CellsPerColumn, 1, config2.SynPermConnected, config2.NumInputs);
+
+            //Compare connectedInputCounter
+            Assert.IsTrue(column7.connectedInputCounter.Equals(column8.connectedInputCounter));
+            Assert.IsFalse(column7.connectedInputCounter.Equals(column9.connectedInputCounter));
 
             //column7 and column8 are same by value 
             Assert.IsTrue(column7.Equals(column8));
 
             //column7 and column9 are NOT same by value (different config)
-            Assert.IsFalse(column7.Equals(column9));
+            Assert.IsFalse(column7.Equals(column9)); 
+            #endregion
 
         }
     }
