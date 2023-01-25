cac08b85a4cf799c71eb68ab295befbce9d95195
FarjanaAkter-hash <shifafarjana634@gmail.com>
2023-01-25 10:10:25 +0100

    Trying to build a TM Test.

---
+++ source/UnitTestsProject/TemporalMemoryTest2.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Damir Dobric. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoCortexApi;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace UnitTestsProject
+{
+    internal class TemporalMemoryTest2
+    {
+    }
+}
