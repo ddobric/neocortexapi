f0a0a2c7dee8f2245eb8ec78a77d5f2bb8f5aeb8
Ayan Borthakur <ayan1948@outlook.com>
2023-01-25 14:02:21 +0100

    Implementation intro.

---
+++ source/NeoCortexApi/Classifiers/KnnClassifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace NeoCortexApi.Classifiers
+{
+    public class KNeighborsClassifier<TIN, TOUT>
+    {
+        private int _nNeighbors;
+        private List<Double[]> _model;
+        
+        
+        KNeighborsClassifier(int nNeighbors = 5)
+        {
+            _nNeighbors = nNeighbors;
+        }
+
+        void KReduceFunc(double[,] dist, int start)
+        {
+        }
+
+        void fit(double[,] x, double[] y)
+        {
+            if (x.Length != y.Length)
+                throw new System.Exception("length of x and y are not same");
+
+            for (int i = 0; i < x.Length; i++)
+                _model.Add(new Double[] { x[i, 0], x[i, 1], y[i] });
+        }
+    }
+}
