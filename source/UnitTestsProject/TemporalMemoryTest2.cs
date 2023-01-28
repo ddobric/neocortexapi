9071dd08147e1ab93a93511e598328324a5c2986
FarjanaAkter-hash <shifafarjana634@gmail.com>
2023-01-28 18:42:38 +0100

    New method added with default parameters changed for testing

--- source/UnitTestsProject/TemporalMemoryTest2.cs
+++ source/UnitTestsProject/TemporalMemoryTest2.cs
@@ -9,7 +9,28 @@ using System.Collections.Generic;
 using System.Linq;
 namespace UnitTestsProject
 {
-    internal class TemporalMemoryTest2
+    [TestClass]
+    public class TemporalMemoryTest2
     {
+        private Parameters getDefaultParameters()
+        {
+            Parameters retVal = Parameters.getTemporalDefaultParameters();
+            retVal.Set(KEY.COLUMN_DIMENSIONS, new int[] { 36 });
+            retVal.Set(KEY.CELLS_PER_COLUMN, 5);
+            retVal.Set(KEY.ACTIVATION_THRESHOLD, 3);
+            retVal.Set(KEY.INITIAL_PERMANENCE, 0.21);
+            retVal.Set(KEY.CONNECTED_PERMANENCE, 0.5);
+            retVal.Set(KEY.MIN_THRESHOLD, 2);
+            retVal.Set(KEY.MAX_NEW_SYNAPSE_COUNT, 3);
+            retVal.Set(KEY.PERMANENCE_INCREMENT, 0.10);
+            retVal.Set(KEY.PERMANENCE_DECREMENT, 0.10);
+            retVal.Set(KEY.PREDICTED_SEGMENT_DECREMENT, 0.0);
+            retVal.Set(KEY.RANDOM, new ThreadSafeRandom(42));
+            retVal.Set(KEY.SEED, 42);
+
+            return retVal;
+        }
+
+
     }
 }
