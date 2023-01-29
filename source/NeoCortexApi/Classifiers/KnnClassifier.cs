ae3a40c42cb42e5c5e7875c7bed3fa09c70c3460
Ayan Borthakur <ayan1948@outlook.com>
2023-01-29 16:37:00 +0100

    Implementation Intro #2

--- source/NeoCortexApi/Classifiers/KnnClassifier.cs
+++ source/NeoCortexApi/Classifiers/KnnClassifier.cs
@@ -1,31 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 
 namespace NeoCortexApi.Classifiers
 {
-    public class KNeighborsClassifier<TIN, TOUT>
+    public class KNeighborsClassifier<TIN, TOUT> : IClassifier<TIN, TOUT>
     {
         private int _nNeighbors;
         private List<Double[]> _model;
-        
-        
+
         KNeighborsClassifier(int nNeighbors = 5)
         {
+            Debug.Assert(nNeighbors % 2 != 0);
             _nNeighbors = nNeighbors;
         }
 
-        void KReduceFunc(double[,] dist, int start)
+
+        double Distance(double[] comparable, double[] dataItem)
         {
+            return Math.Sqrt(Math.Pow(comparable[0] - dataItem[1], 2) - Math.Pow(comparable[1] - dataItem[1], 2));
         }
 
-        void fit(double[,] x, double[] y)
+        int CompareTo()
+
+        void GetPredictedInputValue(double[] dataItem)
         {
-            if (x.Length != y.Length)
+            foreach (var element in _model)
+            {
+            }
+        }
+
+        void Learn(double[,] x, double[] tags)
+        {
+            if (x.Length != tags.Length)
                 throw new System.Exception("length of x and y are not same");
 
             for (int i = 0; i < x.Length; i++)
-                _model.Add(new Double[] { x[i, 0], x[i, 1], y[i] });
+                _model.Add(new Double[] { x[i, 0], x[i, 1], tags[i] });
         }
     }
 }
