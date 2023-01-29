c7c77f606b72ebce4148518cb944d279fcc3995c
Ayan Borthakur <ayan1948@outlook.com>
2023-01-30 00:10:39 +0100

    Trying to use inbuilt datatype to accelerate.

--- source/NeoCortexApi/Classifiers/KnnClassifier.cs
+++ source/NeoCortexApi/Classifiers/KnnClassifier.cs
@@ -8,7 +8,7 @@ namespace NeoCortexApi.Classifiers
     public class KNeighborsClassifier<TIN, TOUT> : IClassifier<TIN, TOUT>
     {
         private int _nNeighbors;
-        private List<Double[]> _model;
+        private double[][] _model;
 
         KNeighborsClassifier(int nNeighbors = 5)
         {
@@ -22,13 +22,15 @@ namespace NeoCortexApi.Classifiers
             return Math.Sqrt(Math.Pow(comparable[0] - dataItem[1], 2) - Math.Pow(comparable[1] - dataItem[1], 2));
         }
 
-        int CompareTo()
 
         void GetPredictedInputValue(double[] dataItem)
         {
-            foreach (var element in _model)
-            {
-            }
+            double[][] distancesIndex = new double[_model.Length][];
+
+            for (var i = 0; i < distancesIndex.Length; i++)
+                distancesIndex[i] = new double[] { Distance(_model[i], dataItem), _model[i][2] };
+            
+            Array.Sort(distancesIndex);
         }
 
         void Learn(double[,] x, double[] tags)
@@ -36,8 +38,10 @@ namespace NeoCortexApi.Classifiers
             if (x.Length != tags.Length)
                 throw new System.Exception("length of x and y are not same");
 
+            _model = new double[x.Length][];
+
             for (int i = 0; i < x.Length; i++)
-                _model.Add(new Double[] { x[i, 0], x[i, 1], tags[i] });
+                _model[i] = new Double[] { x[i, 0], x[i, 1], tags[i] };
         }
     }
 }
