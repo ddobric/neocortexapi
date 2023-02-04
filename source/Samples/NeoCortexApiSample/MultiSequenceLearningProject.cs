32d5e330dd3cabbf5b17086abddfcbd97d22c235
MOHAN SAI RAM SARNALA <116576708+mohansairam117@users.noreply.github.com>
2023-02-04 17:34:31 +0100

    New Prediction Class

--- source/Samples/NeoCortexApiSample/MultiSequenceLearningProject.cs
+++ source/Samples/NeoCortexApiSample/MultiSequenceLearningProject.cs
@@ -10,17 +10,43 @@ using NeoCortexApi.Encoders;
 using NeoCortexApi.Entities;
 using NeoCortexApi.Network;
 
-namespace NeoCortexApiSample
+
+namespace MultiSequencePrediction
 {
-    public class MultiSequenceLearningProject
+    class Program
     {
-        /// <summary>
-        /// Runs the learning of sequences.
-        /// </summary>
-        /// <param name="sequences">Dictionary of sequences. KEY is the sewuence name, the VALUE is th elist of element of the sequence.</param>
-        /*public Predictor RunExperiment(int inputBits, HtmConfig cfg, EncoderBase encoder, Dictionary<string, List<double>> sequences)
+        static void Main(string[] args)
         {
-           
-        }*/
+            List<List<double>> sequences = new List<List<double>>()
+            {
+                new List<double>() {1, 2, 3, 4},
+                new List<double>() {5, 6, 7, 8},
+                new List<double>() {9, 10, 11, 12}
+            };
+
+            int windowSize = 2;
+            List<double> predictions = Predict(sequences, windowSize);
+
+            Console.WriteLine("Predictions: ");
+            predictions.ForEach(p => Console.WriteLine(p));
+        }
+
+        static List<double> Predict(List<List<double>> sequences, int windowSize)
+        {
+            List<double> predictions = new List<double>();
+
+            foreach (var sequence in sequences)
+            {
+                for (int i = windowSize; i < sequence.Count; i++)
+                {
+                    List<double> window = sequence.Skip(i - windowSize).Take(windowSize).ToList();
+                    double prediction = window.Average();
+                    predictions.Add(prediction);
+                }
+            }
+
+            return predictions;
+        }
     }
 }
+
