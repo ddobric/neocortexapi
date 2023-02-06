b987499eb0eb0ca9f565885aafd239ad1c0db9f5
ayan1 <ayan1948@outlook.com>
2023-02-06 15:20:30 +0100

    Added some comments

--- source/NeoCortexApi/Classifiers/KnnClassifier.cs
+++ source/NeoCortexApi/Classifiers/KnnClassifier.cs
@@ -6,6 +6,18 @@ using System.Linq;
 
 namespace NeoCortexApi.Classifiers
 {
+    /// <summary>
+    /// _model: is a list of dictionaries. Which consist of classified data.
+    /// 
+    ///           Value
+    ///         |0 0 1 1|
+    ///   Key = |1 0 0 0|
+    ///         |0 1 1 0|
+    ///      _nNeighbors = 5 by default; which selects the nearest 5 elements.
+    ///
+    /// </summary>
+    /// <typeparam name="TIN"></typeparam>
+    /// <typeparam name="TOUT"></typeparam>
     public class KNeighborsClassifier<TIN, TOUT> : IClassifier<TIN, TOUT>
     {
         private int _nNeighbors;
@@ -17,18 +29,39 @@ namespace NeoCortexApi.Classifiers
             _nNeighbors = nNeighbors;
         }
 
-
-        Dictionary<string, double> GetDistance(int[] comparable, int[] sample)
+        /// <summary>
+        ///     Gets the distance using the equilidean principle using coordinate values (x, y).
+        /// </summary>
+        /// <param name="model">The model with active cells</param>
+        /// <param name="item">The unidentified data with active cells</param>
+        /// <returns>
+        ///     Returns a dataframe containing the cell information model and unidentified
+        ///          Model Row   Model Col   Item Row   Item Col   Distance
+        ///              2          3           3           2         1.65
+        /// </returns>
+        Dictionary<string, double> GetDistance(int[] model, int[] item)
         {
             double distance =
-                Math.Sqrt(Math.Pow(comparable[0] - sample[0], 2) + Math.Pow(comparable[0] - sample[0], 2));
+                Math.Sqrt(Math.Pow(model[0] - item[0], 2) + Math.Pow(model[0] - item[0], 2));
             return new Dictionary<string, double>
             {
-                { "Model Row", comparable[0] }, { "Model Col", comparable[1] }, { "Item Row", sample[0] },
-                { "Item Col", sample[1] }, { "Distance", distance }
+                { "Trained Row", model[0] }, { "Trained Col", model[1] }, { "Test Row", item[0] },
+                { "Test Col", item[1] }, { "Distance", distance }
             };
         }
 
+        /// <summary>
+        ///     Looks for the active cells in the matrix.
+        /// </summary>
+        /// <param name="dataItem">Is the matrix:
+        ///     |0 0 1 1|
+        ///     |1 0 0 0|
+        ///     |0 1 1 0|
+        /// </param>
+        /// <returns>
+        ///     Returns a 2d array containing the cell information of the active regions.
+        ///     A[(0,2),(0,3),(1,0)....]
+        /// </returns>
         List<int[]> GetIndexes(int[][] dataItem)
         {
             List<int[]> coordinates = new List<int[]>();
@@ -44,21 +77,24 @@ namespace NeoCortexApi.Classifiers
             return coordinates;
         }
 
-        void GetPredictedInputValue(int[][] sample)
+        void GetPredictedInputValue(int[][] test)
         {
             foreach (var dict in _model)
             {
-                List<int[]> categoryIndexes = GetIndexes(dict.Value);
-                List<int[]> sampleIndexes = GetIndexes(sample);
-                
+                List<int[]> trainedIndexes = GetIndexes(dict.Value);
+                List<int[]> testIndexes = GetIndexes(test);
+
+                Dictionary<string, List<double>> distanceMatrix;
                 for (int i = 0; i < categoryIndexes.Count; i++)
                 {
                     //TODO: use multiple threads for compute return avg CompareIndexes()
-                    GetDistance(categoryIndexes[i], sampleIndexes[i]);
+                    Dictionary<string, double> distanceDict = GetDistance(trainedIndexes[i], testIndexes[i]);
                 }
             }
         }
-        
-        void Learn(object x, int[] tags){}
+
+        void Learn(object x, int[] tags)
+        {
+        }
     }
 }
