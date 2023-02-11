db304977d4ab8c9d1570df2e3f4c5f562522e145
shiva-kumar-biru <shivabiru98@gmail.com>
2023-02-11 17:40:58 +0100

    code refactoring for file reading

--- source/Samples/NeoCortexApiSample/MultiSequenceLearningProject.cs
+++ source/Samples/NeoCortexApiSample/MultiSequenceLearningProject.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using NeoCortexApi;
@@ -23,15 +24,39 @@ namespace MultiSequencePrediction
             //string path = ".//.//" + System.IO.Directory.GetCurrent‌​Directory();
             string seqPath = @"..\..\..\..\..\MySEProject/trainingSequences.txt";
             string sequencePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), seqPath));
+            sequences = readSequences(sequencePath);
+
+
+
+
+
+            return (null);
+        }
+        public Dictionary<string, List<double>> readSequences(string sequencePath)
+        {
+            Dictionary<string, List<double>> sequences = new Dictionary<string, List<double>>();
             var sequence = new List<double>();
             using (StreamReader reader = new(sequencePath))
             {
                 int count = 1;
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    Console.WriteLine(line);
 
+                    foreach (var value in values)
+                    {
+                        // sequence.Add(Convert.ToDouble(value));
+                        sequence.Add(double.Parse(value));
+                    }
+                    string seqName = "seq" + count;
+                    sequences.Add(seqName, sequence);
+                    count++;
 
+                }
+                return sequences;
             }
-            return(null);
         }
     }
 }
-
