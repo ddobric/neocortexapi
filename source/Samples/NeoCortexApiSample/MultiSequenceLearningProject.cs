65f44f992d4624990baf0aa634d6a18b6dd03691
MOHAN SAI RAM SARNALA <116576708+mohansairam117@users.noreply.github.com>
2023-02-11 17:48:47 +0100

    code refactoring testing sequences

--- source/Samples/NeoCortexApiSample/MultiSequenceLearningProject.cs
+++ source/Samples/NeoCortexApiSample/MultiSequenceLearningProject.cs
@@ -11,7 +11,7 @@ using NeoCortexApi.Classifiers;
 using NeoCortexApi.Encoders;
 using NeoCortexApi.Entities;
 using NeoCortexApi.Network;
-
+using NeoCortexApiSample;
 
 namespace MultiSequencePrediction
 {
@@ -25,9 +25,14 @@ namespace MultiSequencePrediction
             string seqPath = @"..\..\..\..\..\MySEProject/trainingSequences.txt";
             string sequencePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), seqPath));
             sequences = readSequences(sequencePath);
+            MultiSequenceLearning newExperiment = new MultiSequenceLearning();
+            var predictor = newExperiment.Run(sequences);
 
-
-
+            //Testing sequences
+            string tpaths = @"..\..\..\..\..\MySEProject/testingData.txt";
+            string testDataPath = File.ReadAllText(Path.Combine(System.IO.Directory.GetCurrent‌​Directory(), tpaths));
+            var testSequences = new List<List<double>>();
+            var testList = new List<double>();
 
 
             return (null);
