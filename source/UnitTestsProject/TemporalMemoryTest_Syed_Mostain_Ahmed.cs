3ef9404cab31f2341fe6a613433762a6882e54ef
Mostainahmed <mostainahmed@gmail.com>
2023-02-12 22:50:45 +0100

    data changed

--- source/UnitTestsProject/TemporalMemoryTest_Syed_Mostain_Ahmed.cs
+++ source/UnitTestsProject/TemporalMemoryTest_Syed_Mostain_Ahmed.cs
@@ -12,11 +12,22 @@ namespace UnitTestsProject
     [TestClass]
     public class TemporalMemoryTest_Syed_Mostain_Ahmed
     {
+        private static bool areDisjoined<T>(ICollection<T> arr1, ICollection<T> arr2)
+        {
+            foreach (var item in arr1)
+            {
+                if (arr2.Contains(item))
+                    return false;
+            }
+
+            return true;
+        }
+
         private Parameters getDefaultParameters()
         {
             Parameters retVal = Parameters.getTemporalDefaultParameters();
-            retVal.Set(KEY.COLUMN_DIMENSIONS, new int[] { 36 });
-            retVal.Set(KEY.CELLS_PER_COLUMN, 5);
+            retVal.Set(KEY.COLUMN_DIMENSIONS, new int[] { 32 });
+            retVal.Set(KEY.CELLS_PER_COLUMN, 4);
             retVal.Set(KEY.ACTIVATION_THRESHOLD, 3);
             retVal.Set(KEY.INITIAL_PERMANENCE, 0.21);
             retVal.Set(KEY.CONNECTED_PERMANENCE, 0.5);
@@ -35,20 +46,29 @@ namespace UnitTestsProject
         {
             HtmConfig htmConfig = new HtmConfig(new int[] { 32 }, new int[] { 32 })
             {
-                CellsPerColumn = 5,
+                CellsPerColumn = 4,
                 ActivationThreshold = 3,
                 InitialPermanence = 0.21,
                 ConnectedPermanence = 0.5,
                 MinThreshold = 2,
                 MaxNewSynapseCount = 3,
-                PermanenceIncrement = 0.10,
-                PermanenceDecrement = 0.10,
+                PermanenceIncrement = 0.1,
+                PermanenceDecrement = 0.1,
                 PredictedSegmentDecrement = 0,
                 Random = new ThreadSafeRandom(42),
                 RandomGenSeed = 42
             };
 
             return htmConfig;
+        }
+
+
+        private Parameters getDefaultParameters(Parameters p, string key, Object value)
+        {
+            Parameters retVal = p == null ? getDefaultParameters() : p;
+            retVal.Set(key, value);
+
+            return retVal;
         }
 
     }
