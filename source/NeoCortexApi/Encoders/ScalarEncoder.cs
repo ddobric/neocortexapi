c6631c1651d0812447f7c24a0c9b987aa1f5f96e
Gadiraju Anil Kumar <kumarg.mmf@gmail.com>
2023-02-18 12:46:42 +0100

    Code Updates

--- source/NeoCortexApi/Encoders/ScalarEncoder.cs
+++ source/NeoCortexApi/Encoders/ScalarEncoder.cs
@@ -556,20 +556,15 @@ namespace NeoCortexApi.Encoders
                 }        
             }
 
+        public int generateRange Desccriptions(int Ranges)
+        {
+            """generate description from a text description of the ranges"""
+            float desc = " ";
+           
+        }
 
 
-  //def _generateRangeDescription(self, ranges):
-  //  """generate description from a text description of the ranges"""
-  //  desc = ""
-  //  numRanges = len(ranges)
-  //  for i in xrange(numRanges):
-  //    if ranges[i][0] != ranges[i][1]:
-  //      desc += "%.2f-%.2f" % (ranges[i][0], ranges[i][1])
-  //    else:
-  //      desc += "%.2f" % (ranges[i][0])
-  //    if i < numRanges - 1:
-  //      desc += ", "
-  //  return desc
+
 
 
 
