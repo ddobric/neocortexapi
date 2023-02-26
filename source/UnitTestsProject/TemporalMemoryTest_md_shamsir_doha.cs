b749a6ff26372170850b61038e91f2d581c54eb8
Md. Shamsir Doha <shamsirdoha@gmail.com>
2023-02-26 19:43:38 +0100

    testing

--- source/UnitTestsProject/TemporalMemoryTest_md_shamsir_doha.cs
+++ source/UnitTestsProject/TemporalMemoryTest_md_shamsir_doha.cs
@@ -37,12 +37,12 @@ namespace UnitTestsProject
             {
                 CellsPerColumn = 2,
                 ActivationThreshold = 6,
-                InitialPermanence = 0.21,
+                InitialPermanence = 0.8,
                 ConnectedPermanence = 0.1,
                 MinThreshold = 2,
                 MaxNewSynapseCount = 3,
                 PermanenceIncrement = 0.10,
-                PermanenceDecrement = 0.10,
+                PermanenceDecrement = 0.01,
                 PredictedSegmentDecrement = 0,
                 Random = new ThreadSafeRandom(22),
                 RandomGenSeed = 22
@@ -51,8 +51,40 @@ namespace UnitTestsProject
             return htmConfig;
         }
 
-
         [TestMethod]
+        [TestCategory("Prod")]
+        public void TestNoChangeToNonSelectedMatchingSegmentsInBurstingColumn2()
+        {
+            TemporalMemory tm = new TemporalMemory();
+            Connections cn = new Connections();
+            Parameters p = getDefaultParameters(null, KEY.PERMANENCE_DECREMENT, 0.02);
+            p.apply(cn);
+            tm.Init(cn);
+
+            int[] previousActiveColumns = { 0 };
+            int[] activeColumns = { 1 };
+            Cell[] previousActiveCells = { cn.GetCell(0), cn.GetCell(6), cn.GetCell(9), cn.GetCell(3) };
+            Cell[] burstingCells = { cn.GetCell(4), cn.GetCell(5) };
+
+            DistalDendrite selectedMatchingSegment = cn.CreateDistalSegment(burstingCells[0]);
+            cn.CreateSynapse(selectedMatchingSegment, previousActiveCells[0], 0.3);
+            cn.CreateSynapse(selectedMatchingSegment, previousActiveCells[1], 0.3);
+            cn.CreateSynapse(selectedMatchingSegment, previousActiveCells[2], 0.3);
+            cn.CreateSynapse(selectedMatchingSegment, cn.GetCell(81), 0.3);
+
+            DistalDendrite otherMatchingSegment = cn.CreateDistalSegment(burstingCells[1]);
+            Synapse as1 = cn.CreateSynapse(otherMatchingSegment, previousActiveCells[0], 0.7);
+            Synapse as2 = cn.CreateSynapse(otherMatchingSegment, previousActiveCells[1], 0.7);
+            Synapse is1 = cn.CreateSynapse(otherMatchingSegment, cn.GetCell(81), 0.7);
+
+            tm.Compute(previousActiveColumns, true);
+            tm.Compute(activeColumns, true);
+
+            Assert.AreEqual(0.3, as1.Permanence, 0.02);
+            Assert.AreEqual(0.3, as2.Permanence, 0.02);
+            Assert.AreEqual(0.3, is1.Permanence, 0.02);
+
+            [TestMethod]
        
         public void TestPredictedActiveCellsAreAlwaysWinners2()
         {
@@ -68,14 +100,14 @@ namespace UnitTestsProject
             List<Cell> expectedWinnerCells = new List<Cell>(cn.GetCells(new int[] { 4, 9 }));
 
             DistalDendrite activeSegment1 = cn.CreateDistalSegment(expectedWinnerCells[0]);
-            cn.CreateSynapse(activeSegment1, previousActiveCells[0], 0.7);
+            cn.CreateSynapse(activeSegment1, previousActiveCells[0], 0.8);
             cn.CreateSynapse(activeSegment1, previousActiveCells[1], 0.8);
-            cn.CreateSynapse(activeSegment1, previousActiveCells[2], 0.7);
+            cn.CreateSynapse(activeSegment1, previousActiveCells[2], 0.8);
 
             DistalDendrite activeSegment2 = cn.CreateDistalSegment(expectedWinnerCells[1]);
-            cn.CreateSynapse(activeSegment2, previousActiveCells[0], 0.7);
-            cn.CreateSynapse(activeSegment2, previousActiveCells[1], 0.7);
-            cn.CreateSynapse(activeSegment2, previousActiveCells[2], 0.7);
+            cn.CreateSynapse(activeSegment2, previousActiveCells[0], 0.8);
+            cn.CreateSynapse(activeSegment2, previousActiveCells[1], 0.8);
+            cn.CreateSynapse(activeSegment2, previousActiveCells[2], 0.8);
 
             ComputeCycle cc = tm.Compute(previousActiveColumns, false) as ComputeCycle; // learn=false
             cc = tm.Compute(activeColumns, false) as ComputeCycle; // learn=false
