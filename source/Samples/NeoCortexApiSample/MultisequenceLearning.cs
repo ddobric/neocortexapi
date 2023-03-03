7daa5375b170cbd2cb1073af3cdd5aa91b285597
Hungbth2000 <hung.buitranhai@stud.fra-uas.de>
2023-03-03 19:30:34 +0100

    First attempt to Implement Train method for the second training (Issue: Serialization for HtmClassifier is missing)

--- source/Samples/NeoCortexApiSample/MultisequenceLearning.cs
+++ source/Samples/NeoCortexApiSample/MultisequenceLearning.cs
@@ -373,5 +373,212 @@ namespace NeoCortexApiSample
 
             return $"{sequence}_{key}";
         }
+
+        public Predictor RunTraining(Predictor predictor, Dictionary<string, List<double>> sequences)
+        {
+            int inputBits = 100;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            int maxMatchCnt = 0;
+
+            Connections mem = predictor.connections;
+            HtmClassifier<string, ComputeCycle> cls = predictor.classifier;
+            CortexLayer<object, object> layer1 = predictor.layer;
+
+            // The model is first unstable. 
+            bool isInStableState = false;
+
+            // Get the number of unique inputs in the sequences dictionary.
+            var numUniqueInputs = GetNumberOfInputs(sequences);
+
+            var tm = (TemporalMemory)layer1.HtmModules["tm"];
+            var encoder = (ScalarEncoder)layer1.HtmModules["encoder"];
+            var sp = (SpatialPooler)layer1.HtmModules["sp"];
+
+            int[] prevActiveCols = new int[0];
+
+            int cycle = 0;
+            int matches = 0;
+
+            var lastPredictedValues = new List<string>(new string[] { "0" });
+
+            int maxCycles = 3500;
+
+            //
+            // Training SP to get stable. New-born stage.
+            //
+
+            for (int i = 0; i < maxCycles && isInStableState == false; i++)
+            {
+                matches = 0;
+
+                cycle++;
+
+                Debug.WriteLine($"-------------- Newborn Cycle {cycle} ---------------");
+
+                foreach (var inputs in sequences)
+                {
+                    foreach (var input in inputs.Value)
+                    {
+                        Debug.WriteLine($" -- {inputs.Key} - {input} --");
+
+                        var lyrOut = layer1.Compute(input, true);
+
+                        if (isInStableState)
+                            break;
+                    }
+
+                    if (isInStableState)
+                        break;
+                }
+            }
+
+            // Clear all learned patterns in the classifier.
+            cls.ClearState();
+
+            // We activate here the Temporal Memory algorithm.
+            layer1.HtmModules.Add("tm", tm);
+
+            //
+            // Loop over all sequences.
+            foreach (var sequenceKeyPair in sequences)
+            {
+                Debug.WriteLine($"-------------- Sequences {sequenceKeyPair.Key} ---------------");
+
+                int maxPrevInputs = sequenceKeyPair.Value.Count - 1;
+
+                List<string> previousInputs = new List<string>();
+
+                previousInputs.Add("-1.0");
+
+                // Set on true if the system has learned the sequence with a maximum acurracy.
+                bool isLearningCompleted = false;
+
+                //
+                // Now training with SP+TM. SP is pretrained on the given input pattern set.
+                for (int i = 0; i < maxCycles; i++)
+                {
+                    matches = 0;
+
+                    cycle++;
+
+                    Debug.WriteLine("");
+
+                    Debug.WriteLine($"-------------- Cycle {cycle} ---------------");
+                    Debug.WriteLine("");
+
+                    foreach (var input in sequenceKeyPair.Value)
+                    {
+                        Debug.WriteLine($"-------------- {input} ---------------");
+
+                        // Learn the input pattern.
+                        // Output lyrOut is the output of the last module in the layer.
+                        var lyrOut = layer1.Compute(input, true) as ComputeCycle;
+
+                        // This is a general way to get the SpatialPooler result from the layer.
+                        var activeColumns = layer1.GetResult("sp") as int[];
+
+                        previousInputs.Add(input.ToString());
+                        if (previousInputs.Count > (maxPrevInputs + 1))
+                            previousInputs.RemoveAt(0);
+
+                        // In the pretrained SP with HPC, the TM will quickly learn cells for patterns
+                        // In that case the starting sequence 4-5-6 might have the sam SDR as 1-2-3-4-5-6,
+                        // Which will result in returning of 4-5-6 instead of 1-2-3-4-5-6.
+                        // HtmClassifier allways return the first matching sequence. Because 4-5-6 will be as first
+                        // memorized, it will match as the first one.
+                        if (previousInputs.Count < maxPrevInputs)
+                            continue;
+
+                        string key = GetKey(previousInputs, input, sequenceKeyPair.Key);
+
+                        List<Cell> actCells;
+
+                        if (lyrOut.ActiveCells.Count == lyrOut.WinnerCells.Count)
+                        {
+                            actCells = lyrOut.ActiveCells;
+                        }
+                        else
+                        {
+                            actCells = lyrOut.WinnerCells;
+                        }
+
+                        cls.Learn(key, actCells.ToArray());
+
+                        Debug.WriteLine($"Col  SDR: {Helpers.StringifyVector(lyrOut.ActivColumnIndicies)}");
+                        Debug.WriteLine($"Cell SDR: {Helpers.StringifyVector(actCells.Select(c => c.Index).ToArray())}");
+
+                        //
+                        // If the list of predicted values from the previous step contains the currently presenting value,
+                        // we have a match.
+                        if (lastPredictedValues.Contains(key))
+                        {
+                            matches++;
+                            Debug.WriteLine($"Match. Actual value: {key} - Predicted value: {lastPredictedValues.FirstOrDefault(key)}.");
+                        }
+                        else
+                            Debug.WriteLine($"Missmatch! Actual value: {key} - Predicted values: {String.Join(',', lastPredictedValues)}");
+
+                        if (lyrOut.PredictiveCells.Count > 0)
+                        {
+                            //var predictedInputValue = cls.GetPredictedInputValue(lyrOut.PredictiveCells.ToArray());
+                            var predictedInputValues = cls.GetPredictedInputValues(lyrOut.PredictiveCells.ToArray(), 3);
+
+                            foreach (var item in predictedInputValues)
+                            {
+                                Debug.WriteLine($"Current Input: {input} \t| Predicted Input: {item.PredictedInput} - {item.Similarity}");
+                            }
+
+                            lastPredictedValues = predictedInputValues.Select(v => v.PredictedInput).ToList();
+                        }
+                        else
+                        {
+                            Debug.WriteLine($"NO CELLS PREDICTED for next cycle.");
+                            lastPredictedValues = new List<string>();
+                        }
+                    }
+
+                    // The first element (a single element) in the sequence cannot be predicted
+                    double maxPossibleAccuraccy = (double)((double)sequenceKeyPair.Value.Count - 1) / (double)sequenceKeyPair.Value.Count * 100.0;
+
+                    double accuracy = (double)matches / (double)sequenceKeyPair.Value.Count * 100.0;
+
+                    Debug.WriteLine($"Cycle: {cycle}\tMatches={matches} of {sequenceKeyPair.Value.Count}\t {accuracy}%");
+
+                    if (accuracy >= maxPossibleAccuraccy)
+                    {
+                        maxMatchCnt++;
+                        Debug.WriteLine($"100% accuracy reched {maxMatchCnt} times.");
+
+                        //
+                        // Experiment is completed if we are 30 cycles long at the 100% accuracy.
+                        if (maxMatchCnt >= 30)
+                        {
+                            sw.Stop();
+                            Debug.WriteLine($"Sequence learned. The algorithm is in the stable state after 30 repeats with with accuracy {accuracy} of maximum possible {maxMatchCnt}. Elapsed sequence {sequenceKeyPair.Key} learning time: {sw.Elapsed}.");
+                            isLearningCompleted = true;
+                            break;
+                        }
+                    }
+                    else if (maxMatchCnt > 0)
+                    {
+                        Debug.WriteLine($"At 100% accuracy after {maxMatchCnt} repeats we get a drop of accuracy with accuracy {accuracy}. This indicates instable state. Learning will be continued.");
+                        maxMatchCnt = 0;
+                    }
+
+                    // This resets the learned state, so the first element starts allways from the beginning.
+                    tm.Reset(mem);
+                }
+
+                if (isLearningCompleted == false)
+                    throw new Exception($"The system didn't learn with expected acurracy!");
+            }
+
+            Debug.WriteLine("------------ END ------------");
+
+            return new Predictor(layer1, mem, cls);
+        }
     }
 }
