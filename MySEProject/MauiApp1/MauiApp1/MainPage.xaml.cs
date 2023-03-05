ec94d5199d3ebc81bc974f39ccf20f6d89c84436
AnhTong <tongngocminhanh.102@gmail.com>
2023-03-05 22:10:11 +0100

    Input a text file read value, show on screen

--- MySEProject/MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MySEProject/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -1,38 +1,56 @@
-﻿using MauiApp1.ViewModel;
+﻿//using Android.Graphics;
+using MauiApp1.ViewModel;
+using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Storage;
+using DocumentFormat.OpenXml.InkML;
+using System.IO;
+using Microsoft.Maui.Controls;
+using Microsoft.VisualBasic;
 
 namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
 
-	public MainPage(MainViewModel vm)
-	{
-		InitializeComponent();
-		BindingContext = vm;
-	}
-
-}
-/*public async Task<FileResult> PickAndShow(PickOptions options)
-{
-    try
+    public MainPage(MainViewModel vm)
     {
-        var result = await FilePicker.Default.PickAsync(options);
-        if (result != null)
+        InitializeComponent();
+        BindingContext = vm;
+        //// Read the contents of the input text file
+        //string filePath = "A:\\SoftwareProject\\Team_VMAUI\\MySEProject\\MauiApp1\\MauiApp1\\sampleOne.txt";
+        //string fileContents = File.ReadAllText(filePath);
+
+        //// Display the file contents in a Label control
+        //Label fileLabel = new Label
+        //{
+        //    Text = fileContents
+        //};
+
+        //Content = fileLabel;
+    }
+
+    private async void OncouterClicked(object sender, EventArgs e)
+    {
+        var result = await FilePicker.PickAsync(new PickOptions
         {
-            if (result.FileName.EndsWith("txt", StringComparison.OrdinalIgnoreCase))
-            {
-                using var stream = await result.OpenReadAsync();
-                var image = ImageSource.FromStream(() => stream);
-            }
-        }
+            PickerTitle = "pick a text file",
+            FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+        {
+            { DevicePlatform.iOS, new[] { "public.plain-text" } },
+            { DevicePlatform.Android, new[] { "text/plain" } },
+            { DevicePlatform.UWP, new[] { ".txt" } },
+            { DevicePlatform.macOS, new[] { "public.plain-text" } },
+        })
+        });
 
-        return result;
-    }
-    catch (Exception ex)
-    {
-        // The user canceled or something went wrong
-    }
+        if (result == null)
+            return;
 
-    return null;
-}*/
+        var stream = await result.OpenReadAsync();
+        var reader = new StreamReader(stream);
+        var fileContents = await reader.ReadToEndAsync();
+
+        myLabel.Text = fileContents;
+
+    }
+}
