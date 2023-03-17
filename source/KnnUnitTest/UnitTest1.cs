71e2ad0a134065dc54be3e7edb81922d5bd836e6
Ankush Laxman Patil <ankush.patil@stud.fra-uas.de>
2023-03-17 05:01:34 +0100

    Completing the unit test base implemenation

--- source/KnnUnitTest/UnitTest1.cs
+++ source/KnnUnitTest/UnitTest1.cs
@@ -1,11 +1,28 @@
-namespace KnnUnitTest
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+//using NUnit.Framework;
+
+
+namespace KNNClassifier.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class KNNClassifierTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestPredict()
         {
+            // Loading the base data for traning along with labels
+            double[][] trainingData = LoadTrainingData();
+            int[] labels = { 0, 1, 0, 1, 1, 0 };
+            KNNClassifier knn = new KNNClassifier(k: 4);
+            knn.Train(trainingData, labels);
+            double[] testData = { 1.2, 3.4, 5.6, 5.2, 2.2, 1.6 };
+
+            // Predicting the inputs on test data 
+            int predictedLabel = knn.Predict(testData);
+
+            // Preddiction labeling 
+            Assert.AreEqual(1, predictedLabel);
         }
     }
 }
