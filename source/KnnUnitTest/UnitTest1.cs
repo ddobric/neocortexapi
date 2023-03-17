1b7a5926282b6420350c1b5c4eac047a05d0631a
Ankush Laxman Patil <ankush.patil@stud.fra-uas.de>
2023-03-17 05:38:49 +0100

    Completing the Model by integrating local code #Update2

--- source/KnnUnitTest/UnitTest1.cs
+++ source/KnnUnitTest/UnitTest1.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-//using NUnit.Framework;
+using NUnit.Framework;
 
 
 namespace KNNClassifier.Tests
@@ -24,5 +24,17 @@ namespace KNNClassifier.Tests
             // Preddiction labeling 
             Assert.AreEqual(1, predictedLabel);
         }
+        private double[][] LoadTrainingData()
+        {
+            double[][] trainingData = new double[6][];
+            trainingData[0] = new double[] { 0.1, 0.2, 0.3, 1.2, 1.3, 1.4 };
+            trainingData[1] = new double[] { 1.2, 1.3, 1.4, 1.0, 3.0, 4.0 };
+            trainingData[2] = new double[] { 2.3, 2.4, 2.5, 5.2, 3.3, 1.4 };
+            trainingData[3] = new double[] { 3.4, 3.5, 3.6, 0.1, 0.2, 0.3 };
+            trainingData[4] = new double[] { 2.3, 2.4, 2.5, 5.2, 3.3, 1.4 };
+            trainingData[5] = new double[] { 3.4, 3.5, 3.6, 0.1, 0.2, 0.3 };
+            return trainingData;
+        }
+
     }
 }
