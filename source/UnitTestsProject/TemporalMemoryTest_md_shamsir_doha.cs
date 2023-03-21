494c204466e2e987a46f0633fea4d25c734d0681
Md. Shamsir Doha <shamsirdoha@gmail.com>
2023-03-21 09:01:35 +0100

    unnecessary and testing testcase remove

--- source/UnitTestsProject/TemporalMemoryTest_md_shamsir_doha.cs
+++ source/UnitTestsProject/TemporalMemoryTest_md_shamsir_doha.cs
@@ -197,57 +197,7 @@ namespace UnitTestsProject
             Assert.IsNotNull(newSegment);
             Assert.AreEqual(2, newSegment.Synapses.Count);
         }
-
-        [TestMethod]
-        public void TestWinnerCells_top()
-        {
-            TemporalMemory tm = new TemporalMemory();
-            Connections cn = new Connections();
-            Parameters p = getDefaultParameters(null, KEY.MAX_NEW_SYNAPSE_COUNT, 3);
-            p.apply(cn);
-            tm.Init(cn);
-
-            int[] zeroColumns = { };
-            int[] activeColumns = { 0 };
-
-            Cell[] previousActiveCells = { cn.GetCell(0), cn.GetCell(2), cn.GetCell(1), cn.GetCell(2) };
-            tm.Compute(zeroColumns, true);
-            tm.Compute(activeColumns, true);
-
-            Assert.AreEqual(0, cn.NumSegments(), 0);
-        }
-
-        [TestMethod]
-
-       
-        public void TestPredictedActiveCellsAreAlwaysWinners2()
-        {
-            TemporalMemory tm = new TemporalMemory();
-            Connections cn = new Connections();
-            Parameters p = getDefaultParameters();
-            p.apply(cn);
-            tm.Init(cn);
-
-            int[] previousActiveColumns = { 0 };
-            int[] activeColumns = { 1 };
-            Cell[] previousActiveCells = { cn.GetCell(0), cn.GetCell(2), cn.GetCell(1), cn.GetCell(2) };
-            List<Cell> expectedWinnerCells = new List<Cell>(cn.GetCells(new int[] { 4, 9 }));
-
-            DistalDendrite activeSegment1 = cn.CreateDistalSegment(expectedWinnerCells[0]);
-            cn.CreateSynapse(activeSegment1, previousActiveCells[0], 0.8);
-            cn.CreateSynapse(activeSegment1, previousActiveCells[1], 0.8);
-            cn.CreateSynapse(activeSegment1, previousActiveCells[2], 0.8);
-
-            DistalDendrite activeSegment2 = cn.CreateDistalSegment(expectedWinnerCells[1]);
-            cn.CreateSynapse(activeSegment2, previousActiveCells[0], 0.8);
-            cn.CreateSynapse(activeSegment2, previousActiveCells[1], 0.8);
-            cn.CreateSynapse(activeSegment2, previousActiveCells[2], 0.8);
-
-            ComputeCycle cc = tm.Compute(previousActiveColumns, false) as ComputeCycle; // learn=false
-            cc = tm.Compute(activeColumns, false) as ComputeCycle; // learn=false
-
-            Assert.IsFalse(cc.WinnerCells.SequenceEqual(new LinkedHashSet<Cell>(expectedWinnerCells)));
-        }
+        
     }
 }
 // files
