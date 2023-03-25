e4528994ca52e625addbd8491169ddae12ecd66f
pparaska <poonamparaskar2012@gmail.com>
2023-03-25 01:33:57 +0100

    Wrote the accuracy for each sequence in text file

--- source/Samples/NeoCortexApiSample/MultisequenceLearning.cs
+++ source/Samples/NeoCortexApiSample/MultisequenceLearning.cs
@@ -3,13 +3,14 @@ using NeoCortexApi.Classifiers;
 using NeoCortexApi.Encoders;
 using NeoCortexApi.Entities;
 using NeoCortexApi.Network;
-using OfficeOpenXml;
 using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading;
+using OfficeOpenXml;
+using System.IO;
+using OfficeOpenXml.FormulaParsing.LexicalAnalysis;
+
 
 namespace NeoCortexApiSample
 {
@@ -57,7 +58,7 @@ namespace NeoCortexApiSample
                 PredictedSegmentDecrement = 0.1
             };
 
-            double max = 20.0;
+            double max = 20;
 
             Dictionary<string, object> settings = new Dictionary<string, object>()
             {
@@ -81,12 +82,8 @@ namespace NeoCortexApiSample
         /// </summary>
         private Predictor RunExperiment(int inputBits, HtmConfig cfg, EncoderBase encoder, Dictionary<string, List<double>> sequences)
         {
-            List<double> accuracyList = new List<double>();
-            Dictionary<string, double> accuracyPerSequence = new Dictionary<string, double>();
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            int rowNum = 1; // initialize row number to 1
-            int count = 1;
 
             int maxMatchCnt = 0;
 
@@ -135,10 +132,11 @@ namespace NeoCortexApiSample
 
             //double[] inputs = inputValues.ToArray();
             int[] prevActiveCols = new int[0];
+            StreamWriter writer = new StreamWriter("D:\\FUAS\\output.txt");
+
 
             int cycle = 0;
             int matches = 0;
-            double accuracy = 0.0;
 
             var lastPredictedValues = new List<string>(new string[] { "0" });
 
@@ -149,221 +147,166 @@ namespace NeoCortexApiSample
             //
             // Training SP to get stable. New-born stage.
             //
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            using (var package = new ExcelPackage())
-            {
-                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
 
-                for (int i = 0; i < maxCycles && isInStableState == false; i++)
+            for (int i = 0; i < maxCycles && isInStableState == false; i++)
+            {
+                matches = 0;
+
+                cycle++;
+
+                Debug.WriteLine($"-------------- Newborn Cycle {cycle} ---------------");
+
+                foreach (var inputs in sequences)
+                {
+                    foreach (var input in inputs.Value)
+                    {
+                        Debug.WriteLine($" -- {inputs.Key} - {input} --");
+
+                        var lyrOut = layer1.Compute(input, true);
+
+                        if (isInStableState)
+                            break;
+                    }
+
+                    if (isInStableState)
+                        break;
+                }
+            }
+
+            // Clear all learned patterns in the classifier.
+            cls.ClearState();
+
+            // We activate here the Temporal Memory algorithm.
+            layer1.HtmModules.Add("tm", tm);
+
+            //
+            // Loop over all sequences.
+            foreach (var sequenceKeyPair in sequences)
+            {
+                Debug.WriteLine($"-------------- Sequences {sequenceKeyPair.Key} ---------------");
+
+                int maxPrevInputs = sequenceKeyPair.Value.Count - 1;
+
+                List<string> previousInputs = new List<string>();
+
+                previousInputs.Add("-1.0");
+
+                //
+                // Now training with SP+TM. SP is pretrained on the given input pattern set.
+                for (int i = 0; i < maxCycles; i++)
                 {
                     matches = 0;
 
                     cycle++;
 
-                    Debug.WriteLine($"-------------- Newborn Cycle {cycle} ---------------");
+                    Debug.WriteLine("");
 
-                    foreach (var inputs in sequences)
+                    Debug.WriteLine($"-------------- Cycle {cycle} ---------------");
+                    Debug.WriteLine("");
+
+                    foreach (var input in sequenceKeyPair.Value)
                     {
-                        foreach (var input in inputs.Value)
+                        Debug.WriteLine($"-------------- {input} ---------------");
+
+                        var lyrOut = layer1.Compute(input, true) as ComputeCycle;
+
+                        var activeColumns = layer1.GetResult("sp") as int[];
+
+                        previousInputs.Add(input.ToString());
+                        if (previousInputs.Count > (maxPrevInputs + 1))
+                            previousInputs.RemoveAt(0);
+
+                        // In the pretrained SP with HPC, the TM will quickly learn cells for patterns
+                        // In that case the starting sequence 4-5-6 might have the sam SDR as 1-2-3-4-5-6,
+                        // Which will result in returning of 4-5-6 instead of 1-2-3-4-5-6.
+                        // HtmClassifier allways return the first matching sequence. Because 4-5-6 will be as first
+                        // memorized, it will match as the first one.
+                        if (previousInputs.Count < maxPrevInputs)
+                            continue;
+
+                        string key = GetKey(previousInputs, input, sequenceKeyPair.Key);
+
+                        List<Cell> actCells;
+
+                        if (lyrOut.ActiveCells.Count == lyrOut.WinnerCells.Count)
                         {
-                            Debug.WriteLine($" -- {inputs.Key} - {input} --");
-
-                            var lyrOut = layer1.Compute(input, true);
-
-                            if (isInStableState)
-                                break;
+                            actCells = lyrOut.ActiveCells;
+                        }
+                        else
+                        {
+                            actCells = lyrOut.WinnerCells;
                         }
 
-                        if (isInStableState)
+                        cls.Learn(key, actCells.ToArray());
+
+                        Debug.WriteLine($"Col  SDR: {Helpers.StringifyVector(lyrOut.ActivColumnIndicies)}");
+                        Debug.WriteLine($"Cell SDR: {Helpers.StringifyVector(actCells.Select(c => c.Index).ToArray())}");
+
+                        //
+                        // If the list of predicted values from the previous step contains the currently presenting value,
+                        // we have a match.
+                        if (lastPredictedValues.Contains(key))
+                        {
+                            matches++;
+                            Debug.WriteLine($"Match. Actual value: {key} - Predicted value: {lastPredictedValues.FirstOrDefault(key)}.");
+                        }
+                        else
+                            Debug.WriteLine($"Missmatch! Actual value: {key} - Predicted values: {String.Join(',', lastPredictedValues)}");
+
+                        if (lyrOut.PredictiveCells.Count > 0)
+                        {
+                            //var predictedInputValue = cls.GetPredictedInputValue(lyrOut.PredictiveCells.ToArray());
+                            var predictedInputValues = cls.GetPredictedInputValues(lyrOut.PredictiveCells.ToArray(), 3);
+
+                            foreach (var item in predictedInputValues)
+                            {
+                                Debug.WriteLine($"Current Input: {input} \t| Predicted Input: {item.PredictedInput} - {item.Similarity}");
+                            }
+
+                            lastPredictedValues = predictedInputValues.Select(v => v.PredictedInput).ToList();
+                        }
+                        else
+                        {
+                            Debug.WriteLine($"NO CELLS PREDICTED for next cycle.");
+                            lastPredictedValues = new List<string>();
+                        }
+                    }
+
+                    // The first element (a single element) in the sequence cannot be predicted
+                    double maxPossibleAccuraccy = (double)((double)sequenceKeyPair.Value.Count - 1) / (double)sequenceKeyPair.Value.Count * 100.0;
+
+                    double accuracy = (double)matches / (double)sequenceKeyPair.Value.Count * 100.0;
+
+                    writer.WriteLine($"{sequenceKeyPair.Key}, Accuraccy = {accuracy} %");
+                    writer.NewLine= "\n";
+
+                    Debug.WriteLine($"Cycle: {cycle}\tMatches={matches} of {sequenceKeyPair.Value.Count}\t {accuracy}%");
+
+                    if (accuracy >= maxPossibleAccuraccy)
+                    {
+                        maxMatchCnt++;
+                        Debug.WriteLine($"100% accuracy reched {maxMatchCnt} times.");
+
+                        //
+                        // Experiment is completed if we are 30 cycles long at the 100% accuracy.
+                        if (maxMatchCnt >= 30)
+                        {
+                            sw.Stop();
+                            Debug.WriteLine($"Sequence learned. The algorithm is in the stable state after 30 repeats with with accuracy {accuracy} of maximum possible {maxMatchCnt}. Elapsed sequence {sequenceKeyPair.Key} learning time: {sw.Elapsed}.");
                             break;
+                        }
                     }
-                }
-
-                // Clear all learned patterns in the classifier.
-                cls.ClearState();
-
-                // We activate here the Temporal Memory algorithm.
-                layer1.HtmModules.Add("tm", tm);
-
-                //
-                // Loop over all sequences.
-                foreach (var sequenceKeyPair in sequences)
-                {
-                    Debug.WriteLine($"-------------- Sequences {sequenceKeyPair.Key} ---------------");
-
-                    int maxPrevInputs = sequenceKeyPair.Value.Count - 1;
-
-                    List<string> previousInputs = new List<string>();
-
-                    previousInputs.Add("-1.0");
-
-                    //
-                    // Now training with SP+TM. SP is pretrained on the given input pattern set.
-                    for (int i = 0; i < maxCycles; i++)
+                    else if (maxMatchCnt > 0)
                     {
-                        matches = 0;
-
-                        cycle++;
-
-                        Debug.WriteLine("");
-
-                        Debug.WriteLine($"-------------- Cycle {cycle} ---------------");
-                        Debug.WriteLine("");
-
-                        foreach (var input in sequenceKeyPair.Value)
-                        {
-                            Debug.WriteLine($"-------------- {input} ---------------");
-
-                            var lyrOut = layer1.Compute(input, true) as ComputeCycle;
-
-                            var activeColumns = layer1.GetResult("sp") as int[];
-
-                            previousInputs.Add(input.ToString());
-                            if (previousInputs.Count > (maxPrevInputs + 1))
-                                previousInputs.RemoveAt(0);
-
-                            // In the pretrained SP with HPC, the TM will quickly learn cells for patterns
-                            // In that case the starting sequence 4-5-6 might have the sam SDR as 1-2-3-4-5-6,
-                            // Which will result in returning of 4-5-6 instead of 1-2-3-4-5-6.
-                            // HtmClassifier allways return the first matching sequence. Because 4-5-6 will be as first
-                            // memorized, it will match as the first one.
-                            if (previousInputs.Count < maxPrevInputs)
-                                continue;
-
-                            string key = GetKey(previousInputs, input, sequenceKeyPair.Key);
-
-                            List<Cell> actCells;
-
-                            if (lyrOut.ActiveCells.Count == lyrOut.WinnerCells.Count)
-                            {
-                                actCells = lyrOut.ActiveCells;
-                            }
-                            else
-                            {
-                                actCells = lyrOut.WinnerCells;
-                            }
-
-                            cls.Learn(key, actCells.ToArray());
-
-                            Debug.WriteLine($"Col  SDR: {Helpers.StringifyVector(lyrOut.ActivColumnIndicies)}");
-                            Debug.WriteLine($"Cell SDR: {Helpers.StringifyVector(actCells.Select(c => c.Index).ToArray())}");
-
-                            //
-                            // If the list of predicted values from the previous step contains the currently presenting value,
-                            // we have a match.
-                            if (lastPredictedValues.Contains(key))
-                            {
-                                matches++;
-                                Debug.WriteLine($"Match. Actual value: {key} - Predicted value: {lastPredictedValues.FirstOrDefault(key)}.");
-                            }
-                            else
-                                Debug.WriteLine($"Missmatch! Actual value: {key} - Predicted values: {String.Join(',', lastPredictedValues)}");
-
-                            if (lyrOut.PredictiveCells.Count > 0)
-                            {
-                                //var predictedInputValue = cls.GetPredictedInputValue(lyrOut.PredictiveCells.ToArray());
-                                var predictedInputValues = cls.GetPredictedInputValues(lyrOut.PredictiveCells.ToArray(), 3);
-
-                                foreach (var item in predictedInputValues)
-                                {
-                                    Debug.WriteLine($"Current Input: {input} \t| Predicted Input: {item.PredictedInput} - {item.Similarity}");
-                                }
-
-                                lastPredictedValues = predictedInputValues.Select(v => v.PredictedInput).ToList();
-                            }
-                            else
-                            {
-                                Debug.WriteLine($"NO CELLS PREDICTED for next cycle.");
-                                lastPredictedValues = new List<string>();
-                            }
-                        }
-                        String key1;
-                        // The first element (a single element) in the sequence cannot be predicted
-                        double maxPossibleAccuraccy = (double)((double)sequenceKeyPair.Value.Count - 1) / (double)sequenceKeyPair.Value.Count * 100.0;
-
-                        foreach (var key in sequenceKeyPair.Value)
-                        {
-                            accuracy = (double)matches / (double)sequenceKeyPair.Value.Count * 100.0;
-                            
-                            if (!accuracyPerSequence.ContainsKey(sequenceKeyPair.Key))
-                            {
-                                accuracyPerSequence.Add(sequenceKeyPair.Key, accuracy);
-                            }
-                            
-                        }
-
-                        //accuracyList.Add(accuracy);
-                             
-                           
-                        // Save the Excel package to a file
-                        FileInfo fileInfo = new FileInfo("output.xlsx");
-                        package.SaveAs(fileInfo);
-
-
-                        Debug.WriteLine($"{sequenceKeyPair.Key} is having Accuracy: {accuracy}% ");
-
-                        string sequenceKey = sequenceKeyPair.Key;
-                        //double accuracyOfValue = 99.9;
-
-                        // Create a new Excel package and worksheet
-
-
-                       // foreach (var key in sequences.Keys)
-                        //{
-                          //  worksheet.Cells[rowNum, 1].Value = $"{key} is having Accuracy: {accuracy}%";
-                           // rowNum++;
-                        //}
-
-                        // Write the Debug output to the Excel worksheet
-                        // increment row number
-
-                        // Save the Excel package to a file
-                        //FileInfo fileInfo = new FileInfo("output.xlsx");
-                        //package.SaveAs(fileInfo);
-
-
-
-                        Debug.WriteLine($"Cycle: {cycle}\tMatches={matches} of {sequenceKeyPair.Value.Count}\t {accuracy}%");
-
-                        if (accuracy >= maxPossibleAccuraccy)
-                        {
-                            maxMatchCnt++;
-                            Debug.WriteLine($"100% accuracy reched {maxMatchCnt} times.");
-
-                            //
-                            // Experiment is completed if we are 30 cycles long at the 100% accuracy.
-                            if (maxMatchCnt >= 30)
-                            {
-                                sw.Stop();
-                                Debug.WriteLine($"Sequence learned. The algorithm is in the stable state after 30 repeats with with accuracy {accuracy} of maximum possible {maxMatchCnt}. Elapsed sequence {sequenceKeyPair.Key} learning time: {sw.Elapsed}.");
-                                break;
-                            }
-                        }
-                        else if (maxMatchCnt > 0)
-                        {
-                            Debug.WriteLine($"At 100% accuracy after {maxMatchCnt} repeats we get a drop of accuracy with accuracy {accuracy}. This indicates instable state. Learning will be continued.");
-                            maxMatchCnt = 0;
-                        }
-
-                        // This resets the learned state, so the first element starts allways from the beginning.
-                        tm.Reset(mem);
+                        Debug.WriteLine($"At 100% accuracy after {maxMatchCnt} repeats we get a drop of accuracy with accuracy {accuracy}. This indicates instable state. Learning will be continued.");
+                        maxMatchCnt = 0;
                     }
 
-                    //for (int x = 0; x < accuracyList.Count; x++)
-                    //{
-                      //  accuracyPerSequence.Add("Sequence " + count, accuracyList[x]);
-                        //count++;
-                    //}
-                    foreach (KeyValuePair<string, double> kvp in accuracyPerSequence)
-                    {
-                        worksheet.Cells[rowNum, 1].Value = (string.Format("Key = {0}, Value = {1}", kvp.Key, kvp.Value));
-                        // $"{accuracyPerSequence} is having ";
-                        rowNum++;
-                    }
+                    // This resets the learned state, so the first element starts allways from the beginning.
+                    tm.Reset(mem);
                 }
-
-                    
             }
+            writer.Close();
 
             Debug.WriteLine("------------ END ------------");
 
