96bc1c6abdfb85340317ceaa7e38646deb2b249b
Saad Jamil <33003836+saadjamil5@users.noreply.github.com>
2023-03-25 17:02:25 +0500

    Update ScalarEncoder.cs

--- codeSaad/ScalarEncoder.cs
+++ codeSaad/ScalarEncoder.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) Damir Dobric. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+/// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 using NeoCortexApi.Entities;
 using NumSharp;
 using NeoCortexApi.Utility;
