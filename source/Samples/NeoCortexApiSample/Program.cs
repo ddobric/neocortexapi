253fa7d8773ce4c268f4b3a7dda38ff2342b2a5c
Ayan Borthakur <ayan1948@outlook.com>
2023-07-16 22:58:19 +0200

    Optimizing imports and code

--- source/Samples/NeoCortexApiSample/Program.cs
+++ source/Samples/NeoCortexApiSample/Program.cs
@@ -1,10 +1,7 @@
 ﻿using NeoCortexApi;
-using NeoCortexApi.Encoders;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using static NeoCortexApiSample.MultiSequenceLearning;
 
 namespace NeoCortexApiSample
 {
@@ -39,15 +36,14 @@ namespace NeoCortexApiSample
 
         private static void RunMultiSimpleSequenceLearningExperiment()
         {
-            Dictionary<string, List<double>> sequences = new Dictionary<string, List<double>>();
+            var sequences = new Dictionary<string, List<double>>
+            {
+                { "S1", new List<double> { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0 } },
+                { "S2", new List<double> { 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0 } }
+            };
 
-            sequences.Add("S1", new List<double>(new double[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, }));
-            sequences.Add("S2", new List<double>(new double[] { 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0 }));
-
-            //
-            // Prototype for building the prediction engine.
-            MultiSequenceLearning experiment = new MultiSequenceLearning();
-            var predictor = experiment.Run(sequences);         
+            var experiment = new MultiSequenceLearning();
+            var predictor = experiment.Run(sequences);
         }
 
 
@@ -59,35 +55,32 @@ namespace NeoCortexApiSample
         /// </summary>
         private static void RunMultiSequenceLearningExperiment()
         {
-            Dictionary<string, List<double>> sequences = new Dictionary<string, List<double>>();
+            var sequences = new Dictionary<string, List<double>>
+            {
+                { "S1", new List<double>(new double[] { 0.0, 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, }) },
+                { "S2", new List<double>(new double[] { 8.0, 1.0, 2.0, 9.0, 10.0, 7.0, 11.00 }) }
+            };
 
             //sequences.Add("S1", new List<double>(new double[] { 0.0, 1.0, 0.0, 2.0, 3.0, 4.0, 5.0, 6.0, 5.0, 4.0, 3.0, 7.0, 1.0, 9.0, 12.0, 11.0, 12.0, 13.0, 14.0, 11.0, 12.0, 14.0, 5.0, 7.0, 6.0, 9.0, 3.0, 4.0, 3.0, 4.0, 3.0, 4.0 }));
             //sequences.Add("S2", new List<double>(new double[] { 0.8, 2.0, 0.0, 3.0, 3.0, 4.0, 5.0, 6.0, 5.0, 7.0, 2.0, 7.0, 1.0, 9.0, 11.0, 11.0, 10.0, 13.0, 14.0, 11.0, 7.0, 6.0, 5.0, 7.0, 6.0, 5.0, 3.0, 2.0, 3.0, 4.0, 3.0, 4.0 }));
 
-            sequences.Add("S1", new List<double>(new double[] { 0.0, 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, }));
-            sequences.Add("S2", new List<double>(new double[] { 8.0, 1.0, 2.0, 9.0, 10.0, 7.0, 11.00 }));
-
-            //
             // Prototype for building the prediction engine.
-            MultiSequenceLearning experiment = new MultiSequenceLearning();
+            var experiment = new MultiSequenceLearning();
             var predictor = experiment.Run(sequences);
 
             //
             // These list are used to see how the prediction works.
             // Predictor is traversing the list element by element. 
             // By providing more elements to the prediction, the predictor delivers more precise result.
-            var list1 = new double[] { 1.0, 2.0, 3.0, 4.0, 2.0, 5.0 };
-            var list2 = new double[] { 2.0, 3.0, 4.0 };
-            var list3 = new double[] { 8.0, 1.0, 2.0 };
 
             predictor.Reset();
-            PredictNextElement(predictor, list1);
+            PredictNextElement(predictor, new double[] { 1.0, 2.0, 3.0, 4.0, 2.0, 5.0 });
 
             predictor.Reset();
-            PredictNextElement(predictor, list2);
+            PredictNextElement(predictor, new double[] { 2.0, 3.0, 4.0 });
 
             predictor.Reset();
-            PredictNextElement(predictor, list3);
+            PredictNextElement(predictor, new double[] { 8.0, 1.0, 2.0 });
         }
 
         private static void PredictNextElement(Predictor predictor, double[] list)
