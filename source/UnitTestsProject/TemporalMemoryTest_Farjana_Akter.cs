f8069ff20ead0fa08d1ba5c086ec565373fa3d17
Farjana Akter <62109347+farjana-akter-shifa@users.noreply.github.com>
2023-09-01 23:36:45 +0200

    remove cell definition error

--- source/UnitTestsProject/TemporalMemoryTest_Farjana_Akter.cs
+++ source/UnitTestsProject/TemporalMemoryTest_Farjana_Akter.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Damir Dobric. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 using GemBox.Spreadsheet.Tables;
-using IronXL;
 using Microsoft.Azure.Amqp.Framing;
 using Microsoft.Azure.Documents;
 using Microsoft.Extensions.Configuration;
