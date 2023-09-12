596637ba7c7d3198e7debf4ca94f5ecef5e41ba3
FarjanaAkter-hash <shifafarjana634@gmail.com>
2023-09-13 01:15:02 +0200

    Added SEProject reference and MyExperiment project modification.

---
+++ source/MyCloudProjectSample/MyCloudProject.Common/ExerimentRequestMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCloudProject.Common
+{
+    /// <summary>
+    /// Defines the contract for the message request that will run your experiment.
+    /// </summary>
+    public interface IExerimentRequestMessage
+    {
+        string ExperimentId { get; set; }
+
+        string InputFile { get; set; }
+
+        string Name { get; set; }
+
+        string Description { get; set; }
+
+    }
+}
