782777c38dbdafc032492ee1b95deae6b1e705cb
Singari Sahith Kumar <sahithkumar1999@gmail.com>
2023-12-26 11:25:43 +0100

    anomaly detection samples

---
+++ source/Samples/NeoCortexApiAnomaly/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnomalyDetectionSample
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Starts experiment that demonstrates how to perform anomaly detection using multisequencelearning.
+            HTMAnomalyTesting tester = new HTMAnomalyTesting();
+            tester.Run();
+
+        }
+
+    }
+}
