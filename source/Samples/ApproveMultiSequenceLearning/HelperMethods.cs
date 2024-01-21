c6c7825bb98a3350f02b6b3ae4bbffc303b4f36a
Singari Sahith Kumar <sahithkumar1999@gmail.com>
2024-01-21 19:27:17 +0100

    Approve Multisequence Learning

---
+++ source/Samples/ApproveMultiSequenceLearning/HelperMethods.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoCortexApi;
+using NeoCortexApi.Encoders;
+using NeoCortexApi.Entities;
+using MultiSequenceLearning;
+using Newtonsoft.Json;
+
+namespace MultiSequenceLearning
+{
+    public class HelperMethods
+    {
+        public HelperMethods()
+        {
+            //needs no implementation
+        }
+
+        /// <summary>
+        /// HTM Config for creating Connections
+        /// </summary>
+        /// <param name="inputBits">input bits</param>
+        /// <param name="numColumns">number of columns</param>
+        /// <returns>Object of HTMConfig</returns>
+        public static HtmConfig FetchHTMConfig(int inputBits, int numColumns)
+        {
+            HtmConfig cfg = new HtmConfig(new int[] { inputBits }, new int[] { numColumns })
+            {
+                Random = new ThreadSafeRandom(42),
+
+                CellsPerColumn = 25,
+                GlobalInhibition = true,
+                LocalAreaDensity = -1,
+                NumActiveColumnsPerInhArea = 0.02 * numColumns,
+                PotentialRadius = (int)(0.15 * inputBits),
+                //InhibitionRadius = 15,
+
+                MaxBoost = 10.0,
+                DutyCyclePeriod = 25,
+                MinPctOverlapDutyCycles = 0.75,
+                MaxSynapsesPerSegment = (int)(0.02 * numColumns),
+
+                ActivationThreshold = 15,
+                ConnectedPermanence = 0.5,
+
+                // Learning is slower than forgetting in this case.
+                PermanenceDecrement = 0.25,
+                PermanenceIncrement = 0.15,
+
+                // Used by punishing of segments.
+                PredictedSegmentDecrement = 0.1,
+
+                //NumInputs = 88
+            };
+
+            return cfg;
+        }
+
+        /// <summary>
+        /// Takes in user input and return encoded SDR for prediction
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <returns></returns>
+        public static int[] EncodeSingleInput(string userInput)
+        {
+            int[] sdr = new int[0];
+
+            //needs no implementation
+
+            return sdr;
+        }
+
+        /// <summary>
+        /// Get the encoder with settings
+        /// </summary>
+        /// <param name="inputBits">input bits</param>
+        /// <returns>Object of EncoderBase</returns>
+        public static EncoderBase GetEncoder(int inputBits)
+        {
+            double max = 20;
+
+            Dictionary<string, object> settings = new Dictionary<string, object>()
+            {
+                { "W", 15},
+                { "N", inputBits},
+                { "Radius", -1.0},
+                { "MinVal", 0.0},
+                { "Periodic", false},
+                { "Name", "scalar"},
+                { "ClipInput", false},
+                { "MaxVal", max}
+            };
+
+            EncoderBase encoder = new ScalarEncoder(settings);
+
+            return encoder;
+        }
+
+        /// <summary>
+        /// Reads dataset from the file
+        /// </summary>
+        /// <param name="path">full path of the file</param>
+        /// <returns>Object of list of Sequence</returns>
+        public static List<Sequence> ReadDataset(string path)
+        {
+            Console.WriteLine("Reading Sequence...");
+            String lines = File.ReadAllText(path);
+            //var sequence = JsonConvert.DeserializeObject(lines);
+            List<Sequence> sequence = System.Text.Json.JsonSerializer.Deserialize<List<Sequence>>(lines);
+
+            return sequence;
+        }
+
+        /// <summary>
+        /// Creates list of Sequence as per configuration
+        /// </summary>
+        /// <returns>Object of list of Sequence</returns>
+        public static List<Sequence> CreateDataset()
+        {
+            int numberOfSequence = 30;
+            int size = 12;
+            int startVal = 0;
+            int endVal = 15;
+            Console.WriteLine("Creating Sequence...");
+            List<Sequence> sequence = HelperMethods.CreateSequences(numberOfSequence, size, startVal, endVal);
+
+            return sequence;
+        }
+
+        /// <summary>
+        /// Saves the dataset in 'dataset' folder in BasePath of application
+        /// </summary>
+        /// <param name="sequences">Object of list of Sequence</param>
+        /// <returns>Full path of the dataset</returns>
+        public static string SaveDataset(List<Sequence> sequences)
+        {
+            string BasePath = AppDomain.CurrentDomain.BaseDirectory;
+            string reportFolder = Path.Combine(BasePath, "dataset");
+            if (!Directory.Exists(reportFolder))
+                Directory.CreateDirectory(reportFolder);
+            string reportPath = Path.Combine(reportFolder, $"dataset_{DateTime.Now.Ticks}.json");
+
+            Console.WriteLine("Saving dataset...");
+
+            if (!File.Exists(reportPath))
+            {
+                using (StreamWriter sw = File.CreateText(reportPath))
+                {
+                    /*sw.WriteLine("name, data");
+                    foreach (Sequence sequence in sequences)
+                    {
+                        sw.WriteLine($"{sequence.name}, {string.Join(",", sequence.data)}");
+                    }*/
+                    //sw.WriteLine(System.Text.Json.JsonSerializer.Serialize<List<Sequence>>(sequences));
+                    sw.WriteLine(JsonConvert.SerializeObject(sequences));
+                }
+            }
+
+            return reportPath;
+        }
+
+        /// <summary>
+        /// Creats multiple sequences as per parameters
+        /// </summary>
+        /// <param name="count">Number of sequences to be created</param>
+        /// <param name="size">Size of each sequence</param>
+        /// <param name="startVal">Minimum value of item in a sequence</param>
+        /// <param name="stopVal">Maximum value of item in a sequence</param>
+        /// <returns>Object of list of Sequence</returns>
+        public static List<Sequence> CreateSequences(int count, int size, int startVal, int stopVal)
+        {
+            List<Sequence> dataset = new List<Sequence>();
+
+            for (int i = 0; i < count; i++)
+            {
+                Sequence sequence = new Sequence();
+                sequence.name = $"S{i+1}";
+                sequence.data = getSyntheticData(size, startVal, stopVal);
+                dataset.Add(sequence);
+            }
+
+            return dataset;
+        }
+
+        /// <summary>
+        /// Creates a sequence of given size-3 and range
+        /// </summary>
+        /// <param name="size">Size of list</param>
+        /// <param name="startVal">Min range of the list</param>
+        /// <param name="stopVal">Max range of the list</param>
+        /// <returns></returns>
+        private static int[] getSyntheticData(int size, int startVal, int stopVal)
+        {
+            int[] data = new int[size];
+
+            data = randomRemoveDouble(randomDouble(size, startVal, stopVal), 3);
+
+            return data;
+        }
+
+        /// <summary>
+        /// Creates a sorted list of array with given paramerters
+        /// </summary>
+        /// <param name="size">Size of array</param>
+        /// <param name="startVal">Min range of the list</param>
+        /// <param name="stopVal">Max range of the list</param>
+        /// <returns></returns>
+        private static int[] randomDouble(int size, int startVal, int stopVal)
+        {
+            int[] array = new int[size];
+            List<int> list = new List<int>();
+            int number = 0;
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+            while(list.Count < size)
+            {
+                number = r.Next(startVal,stopVal);
+                if (!list.Contains(number))
+                {
+                    if(number >= startVal && number <= stopVal)
+                        list.Add(number);
+                }
+            }
+
+            array = list.ToArray();
+            Array.Sort(array);
+
+            return array;            
+        }
+
+        /// <summary>
+        /// Randomly remove less number of items from array
+        /// </summary>
+        /// <param name="array">array to processed</param>
+        /// <param name="less">number of removals to be done</param>
+        /// <returns>array with less numbers</returns>
+        private static int[] randomRemoveDouble(int[] array, int less)
+        {
+            int[] temp = new int[array.Length - less];
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            int number = 0;
+            List<int> list = new List<int>();
+
+            while (list.Count < (array.Length - less))
+            {
+                number = array[random.Next(0, (array.Length))];
+                if (!list.Contains(number))
+                    list.Add(number);
+            }
+
+            temp = list.ToArray();
+            Array.Sort(temp);
+
+            return temp;
+        }
+
+        private static int getDigits(int n)
+        {
+            if (n >= 0)
+            {
+                if (n < 100) return 2;
+                if (n < 1000) return 3;
+                if (n < 10000) return 4;
+                if (n < 100000) return 5;
+                if (n < 1000000) return 6;
+                if (n < 10000000) return 7;
+                if (n < 100000000) return 8;
+                if (n < 1000000000) return 9;
+                return 10;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+    }
+}
---
+++ source/Samples/ApproveMultiSequenceLearning/Program.cs
@@ -0,0 +1,228 @@
+﻿using NeoCortexApi;
+using NeoCortexApi.Encoders;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using static MultiSequenceLearning.MultiSequenceLearning;
+
+namespace MultiSequenceLearning
+{
+    class Program
+    {
+        /// <summary>
+        /// This sample shows a typical experiment code for SP and TM.
+        /// You must start this code in debugger to follow the trace.
+        /// and TM.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            //to create synthetic dataset
+            /*string path = HelperMethods.SaveDataset(HelperMethods.CreateDataset());
+            Console.WriteLine($"Dataset saved: {path}");*/
+
+            //to read dataset
+            string BasePath = AppDomain.CurrentDomain.BaseDirectory;
+            string datasetPath = Path.Combine(BasePath, "dataset", "dataset_03.json");
+            Console.WriteLine($"Reading Dataset: {datasetPath}");
+            List<Sequence> sequences = HelperMethods.ReadDataset(datasetPath);
+
+            //to read test dataset
+            string testsetPath = Path.Combine(BasePath, "dataset", "test_01.json");
+            Console.WriteLine($"Reading Testset: {testsetPath}");
+            List<Sequence> sequencesTest = HelperMethods.ReadDataset(testsetPath);
+
+            //run learing only
+            //RunSimpleMultiSequenceLearningExperiment(sequences);
+
+            //run learning + prediction and generates report for results
+            List<Report> reports = RunMultiSequenceLearningExperiment(sequences, sequencesTest);
+
+            WriteReport(sequences, reports);
+
+            Console.WriteLine("Done...");
+
+        }
+
+        /// <summary>
+        /// write and formats data in report object to a file
+        /// </summary>
+        /// <param name="sequences">input sequence</param>
+        /// <param name="reports">object of report</param>
+        private static void WriteReport(List<Sequence> sequences, List<Report> reports)
+        {
+            string BasePath = AppDomain.CurrentDomain.BaseDirectory;
+            string reportFolder = Path.Combine(BasePath, "report");
+            if (!Directory.Exists(reportFolder))
+                Directory.CreateDirectory(reportFolder);
+            string reportPath = Path.Combine(reportFolder, $"report_{DateTime.Now.Ticks}.txt");
+
+            if (!File.Exists(reportPath))
+            {
+                using (StreamWriter sw = File.CreateText(reportPath))
+                {
+                    sw.WriteLine("------------------------------");
+                    foreach (Sequence sequence in sequences)
+                    {
+                        sw.WriteLine($"Sequence: {sequence.name} -> {string.Join("-",sequence.data)}");
+                    }
+                    sw.WriteLine("------------------------------");
+                    foreach (Report report in reports)
+                    {
+                        sw.WriteLine($"Using test sequence: {report.SequenceName} -> {string.Join("-",report.SequenceData)}");
+                        foreach (string log in report.PredictionLog)
+                        {
+                            sw.WriteLine($"\t{log}");
+                        }
+                        sw.WriteLine($"\tAccuracy: {report.Accuracy}%");
+                        sw.WriteLine("------------------------------");
+                    }
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// takes input data set and runs the alogrithm
+        /// </summary>
+        /// <param name="sequences">input test dataset</param>
+        private static void RunSimpleMultiSequenceLearningExperiment(List<Sequence> sequences)
+        {
+            //
+            // Prototype for building the prediction engine.
+            MultiSequenceLearning experiment = new MultiSequenceLearning();
+            var predictor = experiment.Run(sequences);
+        }
+
+
+        /// <summary>
+        /// This example demonstrates how to learn two sequences and how to use the prediction mechanism.
+        /// First, two sequences are learned.
+        /// Second, three short sequences with three elements each are created und used for prediction. The predictor used by experiment privides to the HTM every element of every predicting sequence.
+        /// The predictor tries to predict the next element.
+        /// </summary>
+        /// <param name="sequences">input dataset</param>
+        /// <param name="sequencesTest">input test dataset</param>
+        /// <returns>list of Report per sequence</returns>
+        private static List<Report> RunMultiSequenceLearningExperiment(List<Sequence> sequences, List<Sequence> sequencesTest)
+        {
+            List<Report> reports = new List<Report>();
+            Report report = new Report();
+
+            // Prototype for building the prediction engine.
+            MultiSequenceLearning experiment = new MultiSequenceLearning();
+            var predictor = experiment.Run(sequences);
+
+            // These list are used to see how the prediction works.
+            // Predictor is traversing the list element by element. 
+            // By providing more elements to the prediction, the predictor delivers more precise result.
+
+            foreach (Sequence item in sequencesTest)
+            {
+                report.SequenceName = item.name;
+                Debug.WriteLine($"Using test sequence: {item.name}");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine($"Using test sequence: {item.name}");
+                predictor.Reset();
+                report.SequenceData = item.data;
+                var accuracy = PredictNextElement(predictor, item.data, report);
+                reports.Add(report);
+                Console.WriteLine($"Accuracy for {item.name} sequence: {accuracy}%");
+            }
+
+            return reports;
+
+        }
+
+        /// <summary>
+        /// Takes predicted model, subsequence and generates report stating accuracy
+        /// </summary>
+        /// <param name="predictor">Object of Predictor</param>
+        /// <param name="list">sub-sequence to be tested</param>
+        /// <returns>accuracy of predicting elements in %</returns>
+        private static double PredictNextElement(Predictor predictor, int[] list, Report report)
+        {
+            int matchCount = 0;
+            int predictions = 0;
+            double accuracy = 0.0;
+            List<string> logs = new List<string>();
+            Console.WriteLine("------------------------------");
+
+            int prev = -1;
+            bool first = true;
+
+            /*
+             * Pseudo code for calculating accuracy:
+             * 
+             * 1.      loop for each element in the sub-sequence
+             * 1.1     if the element is first element do nothing and save the element as 'previous' for further comparision
+             * 1.2     take previous element and predict the next element
+             * 1.2.1   get the predicted element with highest similarity and compare with 'next' element
+             * 1.2.1.1 if predicted element matches the next element increment the counter of matched elements
+             * 1.2.2   increment the count for number of calls made to predict an element
+             * 1.2     update the 'previous' element with 'next' element
+             * 2.      calculate the accuracy as (number of matched elements)/(total number of calls for prediction) * 100
+             * 3.      end the loop
+             */
+
+            foreach (var next in list)
+            {
+                if(first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Input: {prev}");
+                    var res = predictor.Predict(prev);
+                    string log = "";
+                    if (res.Count > 0)
+                    {
+                        foreach (var pred in res)
+                        {
+                            Debug.WriteLine($"Predicted Input: {pred.PredictedInput} - Similarity: {pred.Similarity}%");
+                        }
+
+                        var sequence = res.First().PredictedInput.Split('_');
+                        var prediction = res.First().PredictedInput.Split('-');
+                        Console.WriteLine($"Predicted Sequence: {sequence.First()} - Predicted next element: {prediction.Last()}");
+                        log = $"Input: {prev}, Predicted Sequence: {sequence.First()}, Predicted next element: {prediction.Last()}";
+                        //compare current element with prediction of previous element
+                        if(next == Int32.Parse(prediction.Last()))
+                        {
+                            matchCount++;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing predicted :(");
+                        log = $"Input: {prev}, Nothing predicted";
+                    }
+
+                    logs.Add(log);
+                    predictions++;
+                }
+
+                //save previous element to compare with upcoming element
+                prev = next;
+            }
+
+            report.PredictionLog = logs;
+
+            /*
+             * Accuracy is calculated as number of matching predictions made 
+             * divided by total number of prediction made for an element in subsequence
+             * 
+             * accuracy = number of matching predictions/total number of prediction * 100
+             */
+            accuracy = (double)matchCount / predictions * 100;
+            report.Accuracy = accuracy;
+            Console.WriteLine("------------------------------");
+
+            return accuracy;
+        }
+    }
+}
