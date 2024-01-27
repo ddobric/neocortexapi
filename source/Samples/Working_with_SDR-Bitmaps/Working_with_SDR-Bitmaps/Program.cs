de46d427a0fc13f264fcb6b938f47b09e0f89af6
yatish0 <yatish_7@hotmail.com>
2024-01-28 02:00:25 +0530

    made some changes

--- source/Samples/Working_with_SDR-Bitmaps/Working_with_SDR-Bitmaps/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World");
-string Name = Console.ReadLine();
-Console.WriteLine($"Project Name - " +Name );
-Console.ReadLine();
