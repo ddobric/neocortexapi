1048e7449ab999d45816c0b20194b1f0379d53fa
Chaitra Bhandari <148810715+chaitra-bhandari@users.noreply.github.com>
2024-01-29 17:57:58 +0100

    Update Program.cs

--- source/MySeProject /Program.cs
+++ source/MySeProject /Program.cs
@@ -10,12 +10,6 @@ namespace NeoCortexApiSample
 {
     class Program
     {
-        /// <summary>
-        /// This sample shows a typical experiment code for SP and TM.
-        /// You must start this code in debugger to follow the trace.
-        /// and TM.
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
             RunMultiSequenceLearningExperiment();
