444a134df6452a903f5c1a2824f58a8501a74423
gauravkale2799 <116576565+gauravkale2799@users.noreply.github.com>
2024-02-11 14:14:56 +0100

    Importing Namespace

--- source/Samples/NeoCortexApiSample/HelpMethod.cs
+++ source/Samples/NeoCortexApiSample/HelpMethod.cs
@@ -1,4 +1,17 @@
 ﻿using System;
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoCortexApi;
+using NeoCortexApi.Encoders;
+using NeoCortexApi.Entities;
+using MultiSequenceLearning;
+using Newtonsoft.Json;
 namespace NeoCortexApiSample
 {
 	public class HelpMethod
