4b7acacde74fdd3810d7205a676237c52fa20899
gauravkale2799 <116576565+gauravkale2799@users.noreply.github.com>
2024-02-11 14:10:59 +0100

    Update HelpMethod.cs
    Created info summary

--- source/Samples/NeoCortexApiSample/HelpMethod.cs
+++ source/Samples/NeoCortexApiSample/HelpMethod.cs
@@ -4,8 +4,18 @@ namespace NeoCortexApiSample
 	public class HelpMethod
 	{
 		public HelpMethod()
-		{
-		}
+        {
+            //needs no implementation
+        }
+       
+      
+
+        /// <summary>
+        /// HTM Config for creating Connections
+        /// </summary>
+        /// <param name="inputBits">input bits</param>
+        /// <param name="numColumns">number of columns</param>
+        /// <returns>Object of HTMConfig</returns>
 	}
 }
 
