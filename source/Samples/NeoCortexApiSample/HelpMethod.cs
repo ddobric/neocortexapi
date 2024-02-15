77291bf6a965bd40785327be61acec96c2629284
gauravkale2799 <116576565+gauravkale2799@users.noreply.github.com>
2024-02-15 21:04:22 +0100

    fetch HTMConfig method from HelperMethods

--- source/Samples/NeoCortexApiSample/HelpMethod.cs
+++ source/Samples/NeoCortexApiSample/HelpMethod.cs
@@ -18,9 +18,9 @@ namespace NeoCortexApiSample
 	public class HelpMethod
 	{
         public HelpMethod();
-       
-       
-      
+
+
+
 
         /// <summary>
         /// HTM Config for creating Connections
@@ -28,6 +28,36 @@ namespace NeoCortexApiSample
         /// <param name="inputBits">input bits</param>
         /// <param name="numColumns">number of columns</param>
         /// <returns>Object of HTMConfig</returns>
-	}
+        ///
+
+
+        public static HtmConfig FetchHTMConfig(int inputBits, int numColumns)
+        {
+            HtmConfig cfg = new HtmConfig(new int[] { inputBits }, new int[] { numColumns })
+            {
+                Random = new ThreadSafeRandom(42),
+
+                CellsPerColumn = 25,
+                GlobalInhibition = true,
+                LocalAreaDensity = -1,
+                NumActiveColumnsPerInhArea = 0.02 * numColumns,
+                PotentialRadius = (int)(0.15 * inputBits),
+                //InhibitionRadius = 15,
+
+                MaxBoost = 10.0,
+                DutyCyclePeriod = 25,
+                MinPctOverlapDutyCycles = 0.75,
+                MaxSynapsesPerSegment = (int)(0.02 * numColumns),
+
+                ActivationThreshold = 15,
+                ConnectedPermanence = 0.5,
+
+              
+            };
+
+            return cfg;
+        }
+    }
+
 }
 
