9049d6e1dfee4de320544b8ca68dceb55795ec5e
HASIBUZZAMAN <hasibniloy14@gmail.com>
2024-02-27 23:55:06 +0100

    trainning file added

---
+++ source/MySEProject/AnomalyDetection1/AnomalyDetection/AnomalyDetector.cs
@@ -0,0 +1,11 @@
+﻿// Example AnomalyDetector.cs file
+using System;
+
+public class AnomalyDetector
+{
+    // Add your anomaly detection methods here
+    public void DetectAnomaly()
+    {
+        // Implement anomaly detection logic
+    }
+}
