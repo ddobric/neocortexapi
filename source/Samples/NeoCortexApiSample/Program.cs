f918ee36336f886cb09e8d7740c3215019b24040
gauravkale2799 <116576565+gauravkale2799@users.noreply.github.com>
2024-02-29 23:26:44 +0530

    Remove Dictionary called sequence

--- source/Samples/NeoCortexApiSample/Program.cs
+++ source/Samples/NeoCortexApiSample/Program.cs
@@ -98,14 +98,7 @@ namespace MultiSequenceLearning
         /// </summary>
         private static void RunMultiSequenceLearningExperiment()
         {
-            Dictionary<string, List<double>> sequences = new Dictionary<string, List<double>>();
-
-            //sequences.Add("S1", new List<double>(new double[] { 0.0, 1.0, 0.0, 2.0, 3.0, 4.0, 5.0, 6.0, 5.0, 4.0, 3.0, 7.0, 1.0, 9.0, 12.0, 11.0, 12.0, 13.0, 14.0, 11.0, 12.0, 14.0, 5.0, 7.0, 6.0, 9.0, 3.0, 4.0, 3.0, 4.0, 3.0, 4.0 }));
-            //sequences.Add("S2", new List<double>(new double[] { 0.8, 2.0, 0.0, 3.0, 3.0, 4.0, 5.0, 6.0, 5.0, 7.0, 2.0, 7.0, 1.0, 9.0, 11.0, 11.0, 10.0, 13.0, 14.0, 11.0, 7.0, 6.0, 5.0, 7.0, 6.0, 5.0, 3.0, 2.0, 3.0, 4.0, 3.0, 4.0 }));
-
-            sequences.Add("S1", new List<double>(new double[] { 0.0, 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, }));
-            sequences.Add("S2", new List<double>(new double[] { 8.0, 1.0, 2.0, 9.0, 10.0, 7.0, 11.00 }));
-
+            
             //
             // Prototype for building the prediction engine.
             MultiSequenceLearning experiment = new MultiSequenceLearning();
