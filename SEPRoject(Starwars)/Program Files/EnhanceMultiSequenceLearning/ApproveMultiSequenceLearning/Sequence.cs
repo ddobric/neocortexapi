578a6ab7e8d9ee8266a73302850d2a5c39fe6353
M-Zubair <Zubairr2024@gmail.com>
2024-03-13 00:00:01 +0100

    Enhance Multisequnc Learning train on Alphabetic sequece

---
+++ SEPRoject(Starwars)/Program Files/EnhanceMultiSequenceLearning/ApproveMultiSequenceLearning/Sequence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiSequenceLearning
+{
+    public class Sequence
+    {
+        public String name { get; set; }
+        public int[] data { get; set; }
+    }
+
+    public class SequenceString
+    {
+        public String name { get; set; }
+        public String data { get; set; }
+    }
+}
