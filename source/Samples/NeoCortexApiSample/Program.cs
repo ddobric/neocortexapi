4f3abf622b59da9558954174b1b8635f1bdb0cea
gauravkale2799 <116576565+gauravkale2799@users.noreply.github.com>
2024-03-12 19:18:37 +0530

    comment unwanted code

--- source/Samples/NeoCortexApiSample/Program.cs
+++ source/Samples/NeoCortexApiSample/Program.cs
@@ -46,8 +46,8 @@ namespace MultiSequenceLearning
         {
             Dictionary<string, List<double>> sequences = new Dictionary<string, List<double>>();
 
-            sequences.Add("S1", new List<double>(new double[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, }));
-            sequences.Add("S2", new List<double>(new double[] { 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0 }));
+            //sequences.Add("S1", new List<double>(new double[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, }));
+            //sequences.Add("S2", new List<double>(new double[] { 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0 }));
 
 
 
