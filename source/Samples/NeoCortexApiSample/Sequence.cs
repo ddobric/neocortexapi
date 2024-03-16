3eede294390f6b9c99c5ff19e0c5d30fb8b11583
gauravkale2799 <116576565+gauravkale2799@users.noreply.github.com>
2024-03-16 22:16:36 +0530

    Create Sequence.cs

---
+++ source/Samples/NeoCortexApiSample/Sequence.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NeoCortexApiSample
+{
+    public class Sequence
+    {
+        public String name { get; set; }
+        public object Name { get; internal set; }
+        public int[] data { get; set; }
+        public int[] Data { get; internal set; }
+
+
+    }
+}
