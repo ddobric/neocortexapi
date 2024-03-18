35a3f28c70a46f07f9fb7f990acdf28422fed35d
gauravkale2799 <116576565+gauravkale2799@users.noreply.github.com>
2024-03-18 22:14:14 +0530

    Create Report.cs

---
+++ source/Samples/NeoCortexApiSample/Report.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace NeoCortexApiSample
+{
+    public class Report
+    {
+        public string SequenceName { get; set; }
+        public int[] SequenceData { get; set; }
+        public List<string> PredictionLog { get; set; }
+        public double Accuracy { get; set; }
+    }
+}
