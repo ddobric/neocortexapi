39e5fe9e6564bf78706f847a00aa61545a7cb0f3
Avradip Mazumdar <rontymazumdar@gmail.com>
2025-01-11 20:22:35 +0100

    Project Folder Creation

---
+++ source/MySEProject/ProjectNeoCortexAPI/ProjectNeoCortexAPI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectNeoCortexAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our project ML 24/25-01");
+            Console.WriteLine("Investigate Image Reconstruction by using Classifiers");
+        }
+    }
+}
