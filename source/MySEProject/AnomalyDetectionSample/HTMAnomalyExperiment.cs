0b052626de2f7bb8c7a0edc76bb16e6c19f9cb79
HasanAhmed-bit <Hasanbc65@gmail.com>
2025-01-11 20:18:18 +0100

    parameters change and code refeactoring

--- source/MySEProject/AnomalyDetectionSample/HTMAnomalyExperiment.cs
+++ source/MySEProject/AnomalyDetectionSample/HTMAnomalyExperiment.cs
@@ -12,11 +12,6 @@ namespace AnomalyDetectionSample
     /// </summary>
     public class HTMAnomalyExperiment
     {
-        private readonly string _trainingCSVFolderPath;
-        private readonly string _predictingCSVFolderPath;
-        private static double _totalAccuracy = 0.0;
-        private static int _iterationCount = 0;
-        private readonly double _tolerance = 0.1;
 
         /// <summary>
         /// Initializes a new instance of the HTMAnomalyExperiment class with default folder paths.
