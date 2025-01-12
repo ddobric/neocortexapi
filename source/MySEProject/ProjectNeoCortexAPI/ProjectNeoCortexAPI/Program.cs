b1c15bc9946b2ecf772ab28025322e52677b27a9
Raka <rakasarkarofficial@gmail.com>
2025-01-13 01:39:45 +0530

    Updating the program file and adding the folder path

--- source/MySEProject/ProjectNeoCortexAPI/ProjectNeoCortexAPI/Program.cs
+++ source/MySEProject/ProjectNeoCortexAPI/ProjectNeoCortexAPI/Program.cs
@@ -4,8 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to our project ML 24/25-01");
-            Console.WriteLine("Investigate Image Reconstruction by using Classifiers");
+            try
+            {
+                Console.WriteLine("Welcome to our project ML 24/25-01");
+                Console.WriteLine("Investigate Image Reconstruction by using Classifiers");
+                Console.WriteLine("Created by:");
+                Console.WriteLine("     Anoushka Piplai[1566664]");
+                Console.WriteLine("     Avradip Mazumdar[1566651]");
+                Console.WriteLine("     Raka Sarkar[1567153]");
+                Console.WriteLine("     Somava Ganguly[1566916]\n\n");
+
+
+                // Specifying the folder path containing images to load for the experiment
+                string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Project_Pictures");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
     }
 }
