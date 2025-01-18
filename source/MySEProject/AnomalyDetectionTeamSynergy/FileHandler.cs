2c0884ef01e29ffb6b9f6a85a98c76691d91ea61
Muhammad-Talha-S <talhasheikh878@gmail.com>
2025-01-18 18:20:34 +0100

    class for handling file operations

---
+++ source/MySEProject/AnomalyDetectionTeamSynergy/FileHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnomalyDetectionTeamSynergy
+{
+    internal class FileHandler
+    {
+        private readonly string defaultTrainingFolder;
+        private readonly string defaultInferringFolder;
+
+        public List<string> TrainingDataFiles { get; private set; } = new List<string>();
+        public List<string> InferringDataFiles { get; private set; } = new List<string>();
+
+        public FileHandler(string defaultTrainingFolder, string defaultInferringFolder)
+        {
+            this.defaultTrainingFolder = defaultTrainingFolder;
+            this.defaultInferringFolder = defaultInferringFolder;
+        }
+    }
+}
