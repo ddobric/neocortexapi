14f97d30239ef6f568fd717e2405eedf5244b89f
HasanAhmed-bit <Hasanbc65@gmail.com>
2025-01-21 11:54:53 +0100

    HTM Model trained

--- source/MySEProject/AnomalyDetectionSample/HTMModeltraining.cs
+++ source/MySEProject/AnomalyDetectionSample/HTMModeltraining.cs
@@ -8,13 +8,7 @@ namespace AnomalyDetectionSample
     {
         public void RunHTMModelLearning(string trainingfolderPath, string predictingfolderPath, out Predictor predictor)
         {
-            Console.WriteLine("------------------------------");
-            Console.WriteLine();
-            Console.WriteLine("Starting our anomaly detection experiment!!");
-            Console.WriteLine();
-            Console.WriteLine("------------------------------");
-            Console.WriteLine();
-            Console.WriteLine("HTM Model training initiated...................");
+
             // Using stopwatch to calculate the total training time
             Stopwatch swh = Stopwatch.StartNew();
 
